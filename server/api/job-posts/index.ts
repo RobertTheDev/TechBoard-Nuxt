dcfbf022cd01a832a8d4fe8380cc042e12461e1b
RobertTheDev <robertthedev@gmail.com>
2023-10-02 14:09:07 +0100

    feat: added winston logger to get job posts

--- server/api/job-posts/index.ts
+++ server/api/job-posts/index.ts
@@ -1,10 +1,14 @@
 import { jobPostsCollection } from "~/server/lib/db/mongodb/collections";
+import winstonLogger from "~/server/lib/logger/winston/winstonLogger";
+
+// This handler gets all job posts from the database.
 
 export default defineEventHandler(async (event) => {
-  const { method } = event.node.req;
+  const { method, originalUrl } = event.node.req;
 
   if (method === "GET") {
     try {
+      // Find and return all the job posts from the db.
       const jobPosts = await jobPostsCollection.find().toArray();
 
       return {
@@ -12,6 +16,11 @@ export default defineEventHandler(async (event) => {
         data: jobPosts,
       };
     } catch (error) {
+      // If an error occurs then log the error and return an unsuccessful statement.
+      const errorMessage = (error as Error).message;
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`
+      );
       return {
         success: false,
       };
