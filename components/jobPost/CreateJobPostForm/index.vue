84469cc984459b69b92d605746180d13dd41d7da
RobertTheDev <robertthedev@gmail.com>
2023-10-02 19:17:37 +0100

    feat: created vee validated form

--- components/jobPost/CreateJobPostForm/index.vue
+++ components/jobPost/CreateJobPostForm/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <p>Create job post form</p>
+    <Form
+      @submit="handleCreateJobPost"
+      :validation-schema="veeValidateZodSchema"
+    >
+      <label>Add title</label>
+      <Field type="text" name="title" />
+      <ErrorMessage name="title" />
+      <button type="submit">Create Job Post</button>
+    </Form>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ErrorMessage, Field, Form } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import createJobPostSchema from "~/models/jobPost/validators/createJobPost.schema";
+
+const veeValidateZodSchema = toTypedSchema(createJobPostSchema);
+
+function handleCreateJobPost(values: any) {
+  alert(JSON.stringify(values, null, 2));
+}
+</script>
