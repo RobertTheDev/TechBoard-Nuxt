ab5623db82790889131698031d83a66103036540
RobertTheDev <robertthedev@gmail.com>
2023-10-02 14:02:28 +0100

    feat: set up mongodb client

---
+++ server/lib/db/mongodb/client.ts
@@ -0,0 +1,10 @@
+import { MongoClient } from "mongodb";
+
+const mongodbUrl = String(process.env.MONGODB_URL);
+const mongodbClient = new MongoClient(mongodbUrl);
+
+const databaseName = "TechBoard";
+
+const mongoClient = mongodbClient.db(databaseName);
+
+export default mongoClient;
