47c4d9ab7ea8480f44434b3f80d4412e3a7238f2
RobertTheDev <robertthedev@gmail.com>
2023-10-02 15:52:19 +0100

    feat: created zod schema for create job post

--- models/jobPost/validators/createJobPost.schema.ts
+++ models/jobPost/validators/createJobPost.schema.ts
@@ -1,9 +1,16 @@
-import { object, date, string } from "yup";
+import { number, object, string, z } from "zod";
 
+// Zod validation schema defines fields required for creating a car.
 const createJobPostSchema = object({
-  companyId: string(),
-  createdAt: date().default(() => new Date()),
-  title: string().notRequired().default(null),
-}).unknown(false);
+  createdAt: z.date().default(() => new Date()),
+  title: string({
+    required_error:
+      "A job title is required. Please provide a title for the job post you are creating.",
+    invalid_type_error: "Job title must be a string.",
+  }).nonempty("Job title cannot be empty."),
+});
+
+// Create a TypeScript type from the schema.
+export type CreateJobPostSchemaType = z.infer<typeof createJobPostSchema>;
 
 export default createJobPostSchema;
