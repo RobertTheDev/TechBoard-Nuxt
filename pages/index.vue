14bd78725c210c1b6a8dce19ae7c5b74fbd29f92
RobertTheDev <robertthedev@gmail.com>
2023-10-02 16:49:45 +0100

    feat: added use fetch to fetch job posts data

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <p>Home Page</p>
+    <p v-for="jobPost in jobPosts" :key="jobPost.id">{{ jobPost.title }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import JobPost from "@/models/jobPost/interfaces/JobPost";
+
+const { data: jobPosts } = await useFetch<JobPost[]>("/api/job-posts");
+</script>
