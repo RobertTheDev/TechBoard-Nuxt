a53b149f3a4dda80f3aad6a6cd40c901dc33b44b
RobertTheDev <robertthedev@gmail.com>
2023-10-02 14:20:59 +0100

    feat: created get job posts route

--- server/api/job-posts/index.ts
+++ server/api/job-posts/index.ts
@@ -1,26 +1,18 @@
-import { jobPostsCollection } from "~/server/lib/db/mongodb/collections";
-import winstonLogger from "~/server/lib/logger/winston/winstonLogger";
-
-// This handler gets all job posts from the database.
+import getJobPosts from "~/server/handlers/jobPost/getJobPosts";
 
 export default defineEventHandler(async (event) => {
-  const { method, originalUrl } = event.node.req;
+  const { method } = event.node.req;
 
+  // This handler gets all job posts from the database.
   if (method === "GET") {
     try {
-      // Find and return all the job posts from the db.
-      const jobPosts = await jobPostsCollection.find().toArray();
+      const jobPosts = await getJobPosts(event);
 
       return {
         success: true,
         data: jobPosts,
       };
     } catch (error) {
-      // If an error occurs then log the error and return an unsuccessful statement.
-      const errorMessage = (error as Error).message;
-      winstonLogger.error(
-        `Error in route ${method} ${originalUrl}: ${errorMessage}`
-      );
       return {
         success: false,
       };
