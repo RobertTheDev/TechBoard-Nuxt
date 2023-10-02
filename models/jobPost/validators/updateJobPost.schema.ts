1c62fd76f35efd5702fbc53e76cfbe8e91d6dca4
RobertTheDev <robertthedev@gmail.com>
2023-10-02 16:06:05 +0100

    feat: created update job post schema using zod

--- models/jobPost/validators/updateJobPost.schema.ts
+++ models/jobPost/validators/updateJobPost.schema.ts
@@ -1,9 +1,16 @@
-import { object, string, date } from "yup";
+import { number, object, string, z } from "zod";
 
+// Zod validation schema defines fields required for creating a car.
 const updateJobPostSchema = object({
-  companyId: string(),
-  updatedAt: date().default(() => new Date()),
-  title: string().optional(),
-}).unknown(false);
+  updatedAt: z.date().default(() => new Date()),
+  title: string({
+    invalid_type_error: "Job title must be a string.",
+  })
+    .nonempty("Job title cannot be empty.")
+    .optional(),
+});
+
+// Create a TypeScript type from the schema.
+export type UpdateJobPostSchemaType = z.infer<typeof updateJobPostSchema>;
 
 export default updateJobPostSchema;
