fbbb1cf5aaca051dfa743f6e763eb43a910654e5
RobertTheDev <robertthedev@gmail.com>
2023-10-02 16:20:21 +0100

    feat: added custom create error

--- server/api/job-posts/index.ts
+++ server/api/job-posts/index.ts
@@ -12,20 +12,14 @@ export default defineEventHandler(async (event) => {
       // Find and return all the job posts from the db.
       const jobPosts = await jobPostsCollection.find().toArray();
 
-      return {
-        success: true,
-        data: jobPosts,
-      };
+      return jobPosts;
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
       winstonLogger.error(
         `Error in route ${method} ${originalUrl}: ${errorMessage}`
       );
-      return {
-        success: false,
-        errorMessage,
-      };
+      return error;
     }
   }
   // This handler validates the body and of successful creates a new job post in the database.
@@ -37,10 +31,10 @@ export default defineEventHandler(async (event) => {
       const validation = await createJobPostSchema.safeParseAsync(body);
 
       if (!validation.success) {
-        return {
-          success: false,
-          message: validation.error.errors[0].message,
-        };
+        throw createError({
+          statusCode: 400,
+          statusMessage: validation.error.errors[0].message,
+        });
       }
 
       // STEP 2: Create a new job post in the database.
@@ -48,25 +42,34 @@ export default defineEventHandler(async (event) => {
         validation.data
       );
 
+      if (!createdJobPost) {
+        throw createError({
+          statusCode: 400,
+          statusMessage:
+            "There was an error trying to create the job post. Please try again.",
+        });
+      }
+
       // STEP 3: Find and return the created job post from the database.
       const jobPost = await jobPostsCollection.findOne({
         _id: new ObjectId(createdJobPost.insertedId),
       });
 
-      return {
-        success: true,
-        data: jobPost,
-      };
+      if (!jobPost) {
+        throw createError({
+          statusCode: 400,
+          statusMessage: "No user was found with the provided id.",
+        });
+      }
+
+      return jobPost;
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
       winstonLogger.error(
         `Error in route ${method} ${originalUrl}: ${errorMessage}`
       );
-      return {
-        success: false,
-        message: errorMessage,
-      };
+      return error;
     }
   }
 });
