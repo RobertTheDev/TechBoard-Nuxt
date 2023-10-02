b48152e43c28d7096bd3cd827fd0618664ca9a39
RobertTheDev <robertthedev@gmail.com>
2023-10-02 21:01:58 +0100

    ci: added css config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@ import componentsConfig from "./components.config";
 
 export default defineNuxtConfig({
   components: componentsConfig,
+  css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
 });
