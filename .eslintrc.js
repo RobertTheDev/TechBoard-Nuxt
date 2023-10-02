de75d3e38b18410d5bb596f12c9571aaaa45fedf
RobertTheDev <robertthedev@gmail.com>
2023-10-02 22:14:52 +0100

    ci: set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vuejs-accessibility/recommended',
+    'plugin:nuxt/recommended',
+    'plugin:security/recommended',
+    'plugin:sonarjs/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+    'vuejs-accessibility',
+    'import',
+    'sonarjs',
+    'vitest',
+  ],
+  rules: {
+    'vue/require-prop-types': 0,
+    'vue/no-duplicate-attributes': 0,
+    'vue/no-parsing-errors': 0,
+  },
+};
