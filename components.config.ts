ca4e170e8ba164e8cbe251ff51723ccb4e9628df
RobertTheDev <robertthedev@gmail.com>
2023-10-02 23:42:58 +0100

    feat: added new component paths

--- components.config.ts
+++ components.config.ts
@@ -1,10 +1,44 @@
 const componentsConfig = [
   {
-    path: '~/components/jobPost',
+    path: '~/components/auth',
     pathPrefix: false,
   },
   {
+    path: '~/components/company',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/companyFollower',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/companyOwner',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/jobApplication',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/jobInterview',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/jobOffer',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/jobPost',
+    pathPrefix: false,
+  },
+
+  {
     path: '~/components/layout',
+    pathPrefix: false,
+  },
+
+  {
+    path: '~/components/savedJobPost',
     pathPrefix: false,
   },
 ];
