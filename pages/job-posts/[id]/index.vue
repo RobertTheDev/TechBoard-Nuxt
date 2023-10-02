3d6d0a26721cabf15e157de1f75bf2352637b627
RobertTheDev <robertthedev@gmail.com>
2023-10-02 17:42:56 +0100

    feat: added use fetch to get job post data

--- pages/job-posts/[id]/index.vue
+++ pages/job-posts/[id]/index.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <p>Job Post</p>
+    <div v-if="jobPost">
+      <p>{{ jobPost.title }}</p>
+    </div>
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import JobPost from "@/models/jobPost/interfaces/JobPost";
+
+const { id } = useRoute().params;
+
+const {
+  data: jobPost,
+  pending,
+  error,
+  refresh,
+} = await useFetch<JobPost | null>(`/api/job-posts/${id}`);
+</script>
