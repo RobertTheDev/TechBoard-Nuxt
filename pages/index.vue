74a6075e933084cd60dc298c74edb0976f96a06a
RobertTheDev <robertthedev@gmail.com>
2023-10-02 17:36:10 +0100

    feat: added pending and error handling

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <p v-for="jobPost in jobPosts" :key="jobPost.id">{{ jobPost.title }}</p>
+    <div v-if="jobPosts">
+      <p v-for="jobPost in jobPosts" :key="jobPost.id">{{ jobPost.title }}</p>
+    </div>
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import JobPost from "@/models/jobPost/interfaces/JobPost";
 
-const { data: jobPosts } = await useFetch<JobPost[]>("/api/job-posts");
+const {
+  data: jobPosts,
+  pending,
+  error,
+  refresh,
+} = await useFetch<JobPost[]>("/api/job-posts");
 </script>
