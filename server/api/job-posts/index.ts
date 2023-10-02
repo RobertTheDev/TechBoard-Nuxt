fb1f12139f22aaafca88947084b7e18399ed271f
RobertTheDev <robertthedev@gmail.com>
2023-10-02 15:55:24 +0100

    feat: added create job post route

--- server/api/job-posts/index.ts
+++ server/api/job-posts/index.ts
@@ -1,7 +1,10 @@
+import { ObjectId } from "mongodb";
+import createJobPostSchema from "~/models/jobPost/validators/createJobPost.schema";
 import { jobPostsCollection } from "~/server/lib/db/mongodb/collections";
+import winstonLogger from "~/server/lib/logger/winston/winstonLogger";
 
 export default defineEventHandler(async (event) => {
-  const { method } = event.node.req;
+  const { method, originalUrl } = event.node.req;
 
   // This handler gets all job posts from the database.
   if (method === "GET") {
@@ -16,9 +19,53 @@ export default defineEventHandler(async (event) => {
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`
+      );
       return {
         success: false,
         errorMessage,
+      };
+    }
+  }
+  // This handler validates the body and of successful creates a new job post in the database.
+  if (method === "POST") {
+    try {
+      // STEP 1: Validate the request body.
+      const body = await readBody(event);
+
+      const validation = await createJobPostSchema.safeParseAsync(body);
+
+      if (!validation.success) {
+        return {
+          success: false,
+          message: validation.error.errors[0].message,
+        };
+      }
+
+      // STEP 2: Create a new job post in the database.
+      const createdJobPost = await jobPostsCollection.insertOne(
+        validation.data
+      );
+
+      // STEP 3: Find and return the created job post from the database.
+      const jobPost = await jobPostsCollection.findOne({
+        _id: new ObjectId(createdJobPost.insertedId),
+      });
+
+      return {
+        success: true,
+        data: jobPost,
+      };
+    } catch (error) {
+      // If an error occurs then log the error and return an unsuccessful statement.
+      const errorMessage = (error as Error).message;
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`
+      );
+      return {
+        success: false,
+        message: errorMessage,
       };
     }
   }
