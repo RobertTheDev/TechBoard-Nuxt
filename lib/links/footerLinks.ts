68e4756e21932618199ba004301c548af88f62d9
RobertTheDev <robertthedev@gmail.com>
2023-10-03 15:28:56 +0100

    feat: added new footer links

--- lib/links/footerLinks.ts
+++ lib/links/footerLinks.ts
@@ -1,7 +1,23 @@
 const footerLinks = [
   {
+    name: 'About',
+    path: '/about',
+  },
+  {
+    name: 'Accessibility Statement',
+    path: '/accessibility-statement',
+  },
+  {
+    name: 'Cookie Policy',
+    path: '/cookie-policy',
+  },
+  {
     name: 'Privacy Policy',
     path: '/privacy-policy',
+  },
+  {
+    name: 'Terms and Conditions',
+    path: '/terms-and-conditions',
   },
 ];
 
