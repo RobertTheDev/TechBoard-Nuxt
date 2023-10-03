cd400b8bb9d86758b139b582f468562904e5f4fd
RobertTheDev <robertthedev@gmail.com>
2023-10-03 14:18:12 +0100

    refactor: changed handler name

--- server/handlers/company/getCompanies.ts
+++ server/handlers/company/getCompanies.ts
@@ -2,6 +2,6 @@ import { companiesCollection } from '~/server/lib/db/mongodb/collections';
 
 // This handler finds and returns all the companies from the db.
 
-export default async function createJobPost() {
+export default async function getCompanies() {
   return await companiesCollection.find().toArray();
 }
