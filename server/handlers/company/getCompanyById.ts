17262949f6868340fc7677488d8b8689d8eca1c1
RobertTheDev <robertthedev@gmail.com>
2023-10-03 14:03:17 +0100

    feat: created get company by id handler

---
+++ server/handlers/company/getCompanyById.ts
@@ -0,0 +1,27 @@
+import { ObjectId } from 'mongodb';
+import {
+  companiesCollection,
+  jobPostsCollection,
+} from '~/server/lib/db/mongodb/collections';
+import { H3Event, EventHandlerRequest } from 'h3';
+
+// This handler finds and returns a company by its unique id from the db.
+
+export default async function getCompanyById(
+  event: H3Event<EventHandlerRequest>,
+) {
+  const { id } = event.context.params as { id: string };
+
+  const company = await companiesCollection.findOne({
+    _id: new ObjectId(id),
+  });
+
+  if (!company) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'No company was found with that id.',
+    });
+  }
+
+  return company;
+}
