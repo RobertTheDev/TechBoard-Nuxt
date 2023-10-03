b8a1236cfa2fa94b2231f6a70b4a84a51b75d0c8
RobertTheDev <robertthedev@gmail.com>
2023-10-03 15:25:02 +0100

    feat: added footer links

--- components/layout/Footer/index.vue
+++ components/layout/Footer/index.vue
@@ -1,10 +1,18 @@
 <template>
   <footer class="bg-white">
     <p>© {{ currentYear }} {{ companyName }}</p>
+    <NuxtLink
+      v-for="footerLink in footerLinks"
+      :to="footerLink.path"
+      :key="footerLink.path"
+    >
+      {{ footerLink.name }}
+    </NuxtLink>
   </footer>
 </template>
 
 <script setup lang="ts">
 import companyName from '~/lib/constants/companyName';
 import currentYear from '~/lib/constants/currentYear';
+import footerLinks from '~/lib/links/footerLinks';
 </script>
