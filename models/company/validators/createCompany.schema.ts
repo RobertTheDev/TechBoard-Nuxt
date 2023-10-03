e054a2ff9f32c7c94434a102da515b608eb218b3
RobertTheDev <robertthedev@gmail.com>
2023-10-03 14:31:44 +0100

    feat: added name field

--- models/company/validators/createCompany.schema.ts
+++ models/company/validators/createCompany.schema.ts
@@ -1,7 +1,12 @@
-import { object, z } from 'zod';
+import { object, string, z } from 'zod';
 
 // Zod validation schema defines fields required for creating a company.
-const createCompanySchema = object({});
+const createCompanySchema = object({
+  name: string({
+    required_error: 'A company name is required.',
+    invalid_type_error: 'Company name must be a string.',
+  }).nonempty('Company name cannot be empty.'),
+});
 
 // Create a TypeScript type from the schema.
 export type CreateCompanySchemaType = z.infer<typeof createCompanySchema>;
