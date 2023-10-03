a3c38d95ddfb7930911aad860658107f93ba632d
RobertTheDev <robertthedev@gmail.com>
2023-10-03 15:09:20 +0100

    feat: created page layout component

---
+++ components/layout/PageLayout/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen min-w-full flex flex-col bg-slate-100">
+    <Header />
+    <div class="flex-1 flex flex-col">
+      <slot />
+    </div>
+    <Footer />
+  </div>
+</template>
