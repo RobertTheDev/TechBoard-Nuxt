17a94cdb4050e16f2620cecc6a01dd3c25b326ee
RobertTheDev <robertthedev@gmail.com>
2023-10-03 18:15:35 +0100

    feat: created handler

--- server/api/companies/[id]/index.ts
+++ server/api/companies/[id]/index.ts
@@ -1,15 +1,14 @@
-import deleteCompanyById from '~/server/handlers/company/deleteCompanyById';
-import getCompanyById from '~/server/handlers/company/getCompanyById';
-import updateCompanyById from '~/server/handlers/company/updateCompanyById';
+import deleteCompanyOwnerById from '~/server/handlers/companyOwner/deleteCompanyOwnerById';
+import getCompanyOwnerById from '~/server/handlers/companyOwner/getCompanyOwnerById';
 import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
 
 export default defineEventHandler(async (event) => {
   const { method, originalUrl } = event.node.req;
 
-  // This handler gets a company by its unique id from the database.
+  // This handler gets a company owner by its unique id from the database.
   if (method === 'GET') {
     try {
-      return await getCompanyById(event);
+      return await getCompanyOwnerById(event);
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
@@ -20,24 +19,10 @@ export default defineEventHandler(async (event) => {
     }
   }
 
-  // This handler deletes a company by its unique id from the database.
+  // This handler deletes a company owner by its unique id from the database.
   if (method === 'DELETE') {
     try {
-      return await deleteCompanyById(event);
-    } catch (error) {
-      // If an error occurs then log the error and return an unsuccessful statement.
-      const errorMessage = (error as Error).message;
-      winstonLogger.error(
-        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
-      );
-      return error;
-    }
-  }
-
-  // This handler updates a company by its unique id from the database.
-  if (method === 'PUT') {
-    try {
-      return await updateCompanyById(event);
+      return await deleteCompanyOwnerById(event);
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
