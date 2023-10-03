5a362293f32629badfbe54e16bb438030414f9cb
RobertTheDev <robertthedev@gmail.com>
2023-10-03 18:16:41 +0100

    feat: created company owners handler routes

---
+++ server/api/company-owners/user/[userId]/index.ts
@@ -0,0 +1,35 @@
+import deleteCompanyOwnersByUserId from '~/server/handlers/companyOwner/deleteCompanyOwnersByUserId';
+import getCompanyOwnersByUserId from '~/server/handlers/companyOwner/getCompanyOwnersByUserId';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  // This handler gets companies owned by their matching user id from the database.
+  if (method === 'GET') {
+    try {
+      return await getCompanyOwnersByUserId(event);
+    } catch (error) {
+      // If an error occurs then log the error and return an unsuccessful statement.
+      const errorMessage = (error as Error).message;
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+      );
+      return error;
+    }
+  }
+
+  // This handler deletes companies owned by their matching user id from the database.
+  if (method === 'DELETE') {
+    try {
+      return await deleteCompanyOwnersByUserId(event);
+    } catch (error) {
+      // If an error occurs then log the error and return an unsuccessful statement.
+      const errorMessage = (error as Error).message;
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+      );
+      return error;
+    }
+  }
+});
