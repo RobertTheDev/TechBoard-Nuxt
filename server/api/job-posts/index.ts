7225ab1e12525931d28f16a47881c4efdda05a17
RobertTheDev <robertthedev@gmail.com>
2023-10-04 15:26:14 +0100

    feat: created job posts route handler

--- server/api/job-posts/index.ts
+++ server/api/job-posts/index.ts
@@ -8,27 +8,30 @@ export default defineEventHandler(async (event) => {
   // This handler gets all job posts from the database.
   if (method === 'GET') {
     try {
-      // Find and return all the job posts from the db.
       return await getJobPosts();
     } catch (error) {
-      // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
+
       winstonLogger.error(
         `Error in route ${method} ${originalUrl}: ${errorMessage}`,
       );
+
       return error;
     }
   }
   // This handler validates the body and of successful creates a new job post in the database.
   if (method === 'POST') {
     try {
-      return await createJobPost(event);
+      const body = await readBody(event);
+
+      return await createJobPost(body);
     } catch (error) {
-      // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
+
       winstonLogger.error(
         `Error in route ${method} ${originalUrl}: ${errorMessage}`,
       );
+
       return error;
     }
   }
