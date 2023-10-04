f6081530aacf635920926d0734b7c1d826a5e230
RobertTheDev <robertthedev@gmail.com>
2023-10-04 17:22:26 +0100

    feat: created settings menu component

---
+++ components/settings/SettingsMenu/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtLink
+      v-for="settingsLink in settingsLinks"
+      :to="settingsLink.path"
+      :key="settingsLink.path"
+    >
+      {{ settingsLink.name }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import settingsLinks from '~/lib/links/settingsLinks';
+</script>
