4f93726f712a70e4ede77f9f4a52c1add6c5a524
RobertTheDev <robertthedev@gmail.com>
2023-10-04 15:27:54 +0100

    feat: created get job posts handler

--- server/handlers/jobPost/getJobPosts.ts
+++ server/handlers/jobPost/getJobPosts.ts
@@ -2,6 +2,6 @@ import { jobPostsCollection } from '~/server/lib/db/mongodb/collections';
 
 // This handler finds and returns all the job posts from the db.
 
-export default async function createJobPost() {
+export default async function getJobPosts() {
   return await jobPostsCollection.find().toArray();
 }
