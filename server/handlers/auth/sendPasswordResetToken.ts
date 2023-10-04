fe2dd8a5a1d73238bb862b2adc65d3f660c0c0ae
RobertTheDev <robertthedev@gmail.com>
2023-10-04 20:26:25 +0100

    feat: created handler

--- server/handlers/auth/sendPasswordResetToken.ts
+++ server/handlers/auth/sendPasswordResetToken.ts
@@ -1,10 +1,10 @@
 import { H3Event, EventHandlerRequest } from 'h3';
 import { usersCollection } from '../../lib/db/mongodb/collections';
-import sendPasswordResetSchema from '~/models/auth/validators/sendPasswordReset.schema';
-import { randomUUID } from 'node:crypto';
-import getUserByEmailAddress from '../user/getUserByEmailAddress';
+import getSessionUser from '../auth/getSessionUser';
+import { ObjectId } from 'mongodb';
+import sendPasswordResetTokenSchema from '~/models/auth/validators/sendPasswordResetToken.schema';
 
-// This handler validates the request body and sends a password reset token.
+// This handler validates the request body and sends the password reset token to user's email.
 
 export default async function sendPasswordResetToken(
   event: H3Event<EventHandlerRequest>,
@@ -12,7 +12,7 @@ export default async function sendPasswordResetToken(
   // STEP 1: Validate the request body.
   const body = await readBody(event);
 
-  const validation = await sendPasswordResetSchema.safeParseAsync(body);
+  const validation = await sendPasswordResetTokenSchema.safeParseAsync(body);
 
   if (!validation.success) {
     throw createError({
@@ -21,57 +21,56 @@ export default async function sendPasswordResetToken(
     });
   }
 
-  const { emailAddress } = validation.data;
+  // STEP 2: Get the user's data.
+  const { _id } = await getSessionUser(event);
 
-  // STEP 2: Find the user by their email address.
-  const signedInUser = await usersCollection.findOne({
-    emailAddress,
+  const user = await usersCollection.findOne({
+    _id: new ObjectId(_id),
   });
 
-  if (!signedInUser) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: `No user with email ${emailAddress} was found in our records.`,
-    });
-  }
-
-  // Check user exists with the requested email address.
-  const user = await getUserByEmailAddress(emailAddress);
-
-  // If no user exists return a 404.
   if (!user) {
     throw createError({
-      statusCode: 404,
-      statusMessage: `Account with email ${emailAddress} does not exist. Please sign up or try again with a different email address.`,
+      statusCode: 400,
+      statusMessage: 'User was not found.',
     });
   }
+
+  // STEP 3: Create the password reset token with expiry.
 
   // Create an expiry time 10 minutes from now.
   const currentTimeInMS = new Date().getTime();
 
-  const resetPasswordTokenExpiryTime = new Date(
+  const passwordResetTokenExpiryTime = new Date(
     currentTimeInMS + 10 * 60 * 1000,
   ).getTime();
 
-  // Create a reset password token using a generated random uuid.
-  const resetPasswordToken = randomUUID();
+  // Create a random UUID to use as a token.
+  const passwordResetToken = crypto.randomUUID();
 
-  // Update the user with the reset password token to retrieve it.
-  await usersCollection.findOneAndUpdate(
+  // STEP 4: Send the password reset email.
+
+  // STEP 5: Add token and expiry to the user in the db.
+  const updatedUser = await usersCollection.findOneAndUpdate(
     {
-      emailAddress,
+      _id: new ObjectId(user._id),
     },
     {
       $set: {
-        resetPasswordToken,
-        resetPasswordTokenExpiryTime,
+        updatedAt: new Date(),
+        passwordResetToken,
+        passwordResetTokenExpiryTime,
       },
     },
   );
 
-  // STEP 5: Return success message.
-  return {
-    statusCode: 200,
-    statusMessage: `Password reset email has been succesfully sent to ${emailAddress}. Please check spam or deleted folder if email does not appear in your inbox.`,
-  };
+  if (!updatedUser) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: `Failed to update user with password reset token. Please try again.`,
+    });
+  }
+
+  // STEP 6: Return success message.
+
+  return 'Succesfully sent password reset email.';
 }
