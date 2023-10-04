8c884c4679e5fcc8856a360c56cbc569af668bb9
RobertTheDev <robertthedev@gmail.com>
2023-10-04 15:25:11 +0100

    feat: created get session route handler

--- server/api/auth/session.get.ts
+++ server/api/auth/session.get.ts
@@ -1,6 +1,19 @@
-import { useSession } from '~/server/lib/session';
+import getSessionUser from '~/server/handlers/auth/getSessionUser';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
 
-export default eventHandler(async (event) => {
-  const session = await useSession(event);
-  return JSON.stringify(session);
+// This handler gets the user from session.
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  try {
+    return await getSessionUser(event);
+  } catch (error) {
+    // If an error occurs then log the error and return an unsuccessful statement.
+    const errorMessage = (error as Error).message;
+    winstonLogger.error(
+      `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+    );
+    return error;
+  }
 });
