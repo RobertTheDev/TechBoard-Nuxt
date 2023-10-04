2e2f1b0f7e4badfc13ee027858734f61916ee4f9
RobertTheDev <robertthedev@gmail.com>
2023-10-04 17:22:00 +0100

    feat: created settings links

---
+++ lib/links/settingsLinks.ts
@@ -0,0 +1,20 @@
+const settingsLinks = [
+  {
+    name: 'Change Email',
+    path: '/settings/change-email',
+  },
+  {
+    name: 'Change Password',
+    path: '/settings/change-password',
+  },
+  {
+    name: 'Verify Email',
+    path: '/settings/verify-email',
+  },
+  {
+    name: 'Close Account',
+    path: '/settings/close-account',
+  },
+];
+
+export default settingsLinks;
