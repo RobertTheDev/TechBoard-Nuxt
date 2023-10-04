70a2720ea47bf73f1ba847e13b09e682ff460caf
RobertTheDev <robertthedev@gmail.com>
2023-10-04 15:26:03 +0100

    feat: created companies route handler

--- server/api/companies/index.ts
+++ server/api/companies/index.ts
@@ -5,30 +5,31 @@ import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
 export default defineEventHandler(async (event) => {
   const { method, originalUrl } = event.node.req;
 
-  // This handler gets all companies from the database.
   if (method === 'GET') {
     try {
-      // Find and return all the companies from the db.
       return await getCompanies();
     } catch (error) {
-      // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
+
       winstonLogger.error(
         `Error in route ${method} ${originalUrl}: ${errorMessage}`,
       );
+
       return error;
     }
   }
-  // This handler validates the body and of successful creates a new company in the database.
   if (method === 'POST') {
     try {
-      return await createCompany(event);
+      const body = await readBody(event);
+
+      return await createCompany(body);
     } catch (error) {
-      // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
+
       winstonLogger.error(
         `Error in route ${method} ${originalUrl}: ${errorMessage}`,
       );
+
       return error;
     }
   }
