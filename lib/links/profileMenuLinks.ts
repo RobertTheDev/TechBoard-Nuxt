ea2d4e4b4f85a0188fa130de7485c832b998afe6
RobertTheDev <robertthedev@gmail.com>
2023-10-04 17:35:29 +0100

    feat: created profile menu links

---
+++ lib/links/profileMenuLinks.ts
@@ -0,0 +1,20 @@
+const profileMenuLinks = [
+  {
+    name: 'Profile',
+    path: '/profile',
+  },
+  {
+    name: 'Edit Profile',
+    path: '/profile/edit-profile',
+  },
+  {
+    name: 'My Companies',
+    path: '/profile/my-companies',
+  },
+  {
+    name: 'Settings',
+    path: '/settings/change-email',
+  },
+];
+
+export default profileMenuLinks;
