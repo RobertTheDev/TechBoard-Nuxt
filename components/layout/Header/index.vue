7345b85536cdffe3ac281b4c42db187f9c85b5ea
RobertTheDev <robertthedev@gmail.com>
2023-10-04 15:29:34 +0100

    feat: added auth handlers

--- components/layout/Header/index.vue
+++ components/layout/Header/index.vue
@@ -2,9 +2,31 @@
   <header class="bg-white">
     <NuxtLink to="/">{{ companyName }}</NuxtLink>
     <NuxtLink to="/job-posts/create">Create Job Post</NuxtLink>
+    <p v-if="user">{{ user.emailAddress }}</p>
+    <button v-if="user" @click="handleSignOut">Sign Out</button>
+    <button v-if="!user">Sign In</button>
+    <NuxtLink to="/auth/sign-in">Sign In</NuxtLink>
   </header>
 </template>
 
 <script setup lang="ts">
 import companyName from '~/lib/constants/companyName';
+
+async function handleSignOut() {
+  const { pending, error } = await useFetch(`/api/auth/sign-out`);
+  if (pending.value) {
+    console.log('loading');
+  } else if (error.value) {
+    console.log(error.value);
+  } else {
+    window.location.reload();
+  }
+}
+
+const {
+  data: user,
+  pending,
+  error,
+  refresh,
+} = await useFetch<{ id: string; emailAddress: string }>('/api/auth/session');
 </script>
