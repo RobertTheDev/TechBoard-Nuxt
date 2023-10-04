5a8a0e7f66e0a776905ddbd44c730a988b01ffe5
RobertTheDev <robertthedev@gmail.com>
2023-10-04 18:06:19 +0100

    feat: created send token route handler

---
+++ server/api/auth/reset-password/send-token.post.ts
@@ -0,0 +1,18 @@
+import getSessionUser from '~/server/handlers/auth/getSessionUser';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  try {
+    return await getSessionUser(event);
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+
+    winstonLogger.error(
+      `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+    );
+
+    return error;
+  }
+});
