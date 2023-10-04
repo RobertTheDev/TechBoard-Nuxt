2dd717950586ad935ce070d0bd2650bb3be3c9ed
RobertTheDev <robertthedev@gmail.com>
2023-10-04 15:22:41 +0100

    ci: deleted files

--- server/api/auth/logout.get.ts
+++
@@ -1,7 +0,0 @@
-import { useSession } from '~/server/lib/session';
-
-export default eventHandler(async (event) => {
-  const session = await useSession(event);
-  await session.destroy();
-  return 'OK';
-});
