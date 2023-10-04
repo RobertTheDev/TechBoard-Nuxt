2d32233bc6c98d8099c187e0f8bf83056a3374dc
RobertTheDev <robertthedev@gmail.com>
2023-10-04 18:49:08 +0100

    feat: added fields to schema

--- models/company/validators/updateCompany.schema.ts
+++ models/company/validators/updateCompany.schema.ts
@@ -1,7 +1,15 @@
 import { object, z } from 'zod';
 
 // Zod validation schema defines fields required for updating a company.
-const updateCompanySchema = object({});
+const updateCompanySchema = object({
+  name: z
+    .string({
+      invalid_type_error: 'Company name must be a string.',
+      required_error: 'Company name is required.',
+    })
+    .nonempty('Company name cannot be named.')
+    .optional(),
+});
 
 // Create a TypeScript type from the schema.
 export type UpdateCompanySchemaType = z.infer<typeof updateCompanySchema>;
