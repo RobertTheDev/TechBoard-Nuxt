c3318e44c6cab8b0394f2d1fc1050f3721ddcda3
RobertTheDev <robertthedev@gmail.com>
2023-10-05 01:27:43 +0100

    feat: added send password reset token handler

--- server/api/auth/reset-password/send-token.post.ts
+++ server/api/auth/reset-password/send-token.post.ts
@@ -1,11 +1,13 @@
-import getSessionUser from '~/server/handlers/auth/getSessionUser';
+import sendPasswordResetToken from '~/server/handlers/auth/sendPasswordResetToken';
 import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+// This handler sends a password reset token to user's email.
 
 export default defineEventHandler(async (event) => {
   const { method, originalUrl } = event.node.req;
 
   try {
-    return await getSessionUser(event);
+    return await sendPasswordResetToken(event);
   } catch (error) {
     const errorMessage = (error as Error).message;
 
