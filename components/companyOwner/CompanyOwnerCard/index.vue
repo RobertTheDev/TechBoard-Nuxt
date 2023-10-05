fe2b933a8da9e93b7451d3f29bf4a159c2fdac51
RobertTheDev <robertthedev@gmail.com>
2023-10-06 00:39:24 +0100

    feat: created company owner card component

---
+++ components/companyOwner/CompanyOwnerCard/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>{{ companyOwner._id }}</p>
+    <p>{{ companyOwner.company.name }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ICompanyOwner from '~/models/companyOwner/interfaces/CompanyOwner';
+
+const companyOwner = defineProps<ICompanyOwner>();
+</script>
