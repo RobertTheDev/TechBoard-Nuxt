a7baccfdba188c535943cb6b4555b6234cad1e76
RobertTheDev <robertthedev@gmail.com>
2023-10-05 13:34:42 +0100

    feat: created route handler

---
+++ server/api/settings/verify-email/send-token.post.ts
@@ -0,0 +1,20 @@
+import sendEmailVerificationToken from '~/server/handlers/settings/sendEmailVerificationToken';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+// This handler sends an email verification token to user's email address.
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  try {
+    return await sendEmailVerificationToken(event);
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+
+    winstonLogger.error(
+      `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+    );
+
+    return error;
+  }
+});
