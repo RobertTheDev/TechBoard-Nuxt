c2888185fd613f5e4c15a0d07ae9606dd6eb900e
RobertTheDev <robertthedev@gmail.com>
2023-10-05 14:57:42 +0100

    feat: added token management handlers

--- server/handlers/settings/verifyEmailVerificationToken.ts
+++ server/handlers/settings/verifyEmailVerificationToken.ts
@@ -1,67 +1,67 @@
 import { H3Event, EventHandlerRequest } from 'h3';
 import { usersCollection } from '../../lib/db/mongodb/collections';
-import getSessionUser from '../auth/getSessionUser';
-import { ObjectId } from 'mongodb';
-import sendEmailVerificationTokenSchema from '~/models/settings/validators/sendEmailVerificationTokenSchema';
+import isAuthenticated from '../auth/isAuthenticated';
+import isTokenExpired from '~/server/lib/tokenManagement/isTokenExpired';
 
 // This handler matches user with the email verifcation token and verifies them.
 
 export default async function verifyEmailVerificationToken(
   event: H3Event<EventHandlerRequest>,
 ) {
-  // STEP 1: Get the token from the params.
-  const body = await readBody(event);
+  // STEP 1: Check user is authenticated.
+  await isAuthenticated(event);
 
-  // STEP 2: Get the user's data.
-  const { _id } = await getSessionUser(event);
+  // STEP 2: Find the user with the token from params.
+  const { emailVerificationToken } = event.context.params as {
+    emailVerificationToken: string;
+  };
 
   const user = await usersCollection.findOne({
-    _id: new ObjectId(_id),
+    emailVerificationToken,
   });
 
   if (!user) {
     throw createError({
       statusCode: 400,
-      statusMessage: 'User was not found.',
+      statusMessage: 'Could not find a user with that verification token.',
     });
   }
 
-  // STEP 3: Create the verification token has not expired.
+  // STEP 3: Check token has not expired.
+  const checkTokenExpired = await isTokenExpired(
+    user.emailVerificationTokenExpiryTime,
+  );
 
-  // Create an expiry time 10 minutes from now.
-  const currentTimeInMS = new Date().getTime();
+  if (checkTokenExpired) {
+    throw createError({
+      statusCode: 400,
+      statusMessage:
+        'Your email verification token has expired. Please request for a new one.',
+    });
+  }
 
-  const emailVerificationTokenExpiryTime = new Date(
-    currentTimeInMS + 10 * 60 * 1000,
-  ).getTime();
-
-  // Create a random UUID to use as a token.
-  const emailVerificationToken = crypto.randomUUID();
-
-  // STEP 4: Send the email verification email.
-
-  // STEP 5: Add token and expiry to the user in the db.
-  const updatedUser = await usersCollection.findOneAndUpdate(
+  // STEP 4: Verify the user email.
+  const verifiedUser = await usersCollection.findOneAndUpdate(
     {
-      _id: new ObjectId(user._id),
+      emailVerificationToken,
     },
     {
       $set: {
         updatedAt: new Date(),
-        emailVerificationToken,
-        emailVerificationTokenExpiryTime,
+        emailVerified: new Date(),
+        emailVerificationToken: null,
+        emailVerificationTokenExpiryTime: null,
       },
     },
   );
 
-  if (!updatedUser) {
+  if (!verifiedUser) {
     throw createError({
       statusCode: 400,
-      statusMessage: `Failed to update user with verification token. Please try again.`,
+      statusMessage: 'Failed to verify your email address. Please try again.',
     });
   }
 
-  // STEP 6: Return success message.
-
-  return 'Succesfully sent email verification email.';
+  // STEP 7: Send success message.
+  return 'Succesfully verified your email address.';
 }
