51ce2f9838b3440cf96c4c0284e7b9907d2de5a0
RobertTheDev <robertthedev@gmail.com>
2023-10-06 00:23:26 +0100

    feat: added company owners fetch handler

--- pages/profile/my-companies.vue
+++ pages/profile/my-companies.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
     <h1>My Companies</h1>
+    <button>Create Company</button>
+    <CreateCompanyForm />
+    <div v-if="!companies">
+      <p>You have no companies yet.</p>
+    </div>
+    <div v-if="companies">
+      <div v-for="company in companies">
+        <p>{{ company._id }}</p>
+        <p>{{ company.company.name }}</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ICompanyOwner from '~/models/companyOwner/interfaces/CompanyOwner';
+
+const {
+  data: companies,
+  pending,
+  error,
+  refresh,
+} = await useFetch<ICompanyOwner[]>('/api/company-owners/authenticated-user');
+</script>
