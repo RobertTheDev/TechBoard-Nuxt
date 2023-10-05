04ee92fd7cee52a39b9726c3d02393cf91b4cded
RobertTheDev <robertthedev@gmail.com>
2023-10-06 00:23:08 +0100

    feat: added company and user fields

--- models/companyOwner/interfaces/CompanyOwner.ts
+++ models/companyOwner/interfaces/CompanyOwner.ts
@@ -1,5 +1,12 @@
+import ICompany from '~/models/company/interfaces/Company';
+import IUser from '~/models/user/interfaces/User';
+
 export default interface ICompanyOwner {
   _id: string;
   createdAt: string;
   updatedAt?: Date;
+  companyId: string;
+  userId: string;
+  company: ICompany;
+  user: IUser;
 }
