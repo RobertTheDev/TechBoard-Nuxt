efd6aacf1133667f1d5b5638aec15bcbf088d9eb
RobertTheDev <robertthedev@gmail.com>
2023-10-05 23:45:31 +0100

    feat: added get profle fetch handler

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <h1>Profile</h1>
+    <div v-if="profile">
+      <p>{{ profile.emailAddress }}</p>
+      <p>{{ profile.firstName }} {{ profile.lastName }}</p>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import IProfile from '~/models/profile/interfaces/Profile';
+
+const {
+  data: profile,
+  pending,
+  error,
+  refresh,
+} = await useFetch<IProfile>('/api/profile');
+</script>
