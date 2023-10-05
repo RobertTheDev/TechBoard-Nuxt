60780dda8cd2505943249f4e7a32fc23a0175f80
RobertTheDev <robertthedev@gmail.com>
2023-10-05 13:29:14 +0100

    feat: added not found slot

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,9 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <h1>404 - Not Found</h1>
+      </template>
+    </ContentDoc>
   </main>
 </template>
