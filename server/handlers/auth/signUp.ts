d7dc7e826b3daf7f10f7f5a8171b9e5546aceaf2
RobertTheDev <robertthedev@gmail.com>
2023-10-05 01:25:52 +0100

    feat: added password hashing

--- server/handlers/auth/signUp.ts
+++ server/handlers/auth/signUp.ts
@@ -3,11 +3,15 @@ import { usersCollection } from '../../lib/db/mongodb/collections';
 import signUpSchema from '~/models/auth/validators/signUp.schema';
 import { useSession } from '~/server/lib/session';
 import hashPassword from '~/server/lib/passwordManagement/hashPassword';
+import isSignedIn from './isSignedIn';
 
 // This handler validates the request body and signs up a new user and signs them into session.
 
 export default async function signUp(event: H3Event<EventHandlerRequest>) {
-  // STEP 1: Validate the request body.
+  // STEP 1: Check if user is signed in.
+  await isSignedIn(event);
+
+  // STEP 2: Validate the request body.
   const body = await readBody(event);
 
   const validation = await signUpSchema.safeParseAsync(body);
@@ -19,7 +23,7 @@ export default async function signUp(event: H3Event<EventHandlerRequest>) {
     });
   }
 
-  // STEP 2: Check email address is not in use.
+  // STEP 3: Check email address is not in use.
   const { emailAddress } = validation.data;
 
   const findUserByEmailAddress = await usersCollection.findOne({
@@ -33,12 +37,19 @@ export default async function signUp(event: H3Event<EventHandlerRequest>) {
     });
   }
 
-  // STEP 3: Hash the user's password.
+  // STEP 4: Hash the user's password.
   const { password, ...userDataWithoutPassword } = validation.data;
 
   const hashedPassword = await hashPassword(password);
 
-  // STEP 4: Creates or signs up a new user in the database.
+  if (!hashedPassword) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Failed to hash the password. Please try again',
+    });
+  }
+
+  // STEP 5: Creates or signs up a new user in the database.
   const signedUpUser = await usersCollection.insertOne({
     createdAt: new Date(),
     ...userDataWithoutPassword,
@@ -53,13 +64,13 @@ export default async function signUp(event: H3Event<EventHandlerRequest>) {
     });
   }
 
-  // STEP 5: Sign the user into session.
+  // STEP 6: Sign the user into session.
   const session = await useSession(event);
 
   session.user = userDataWithoutPassword;
 
   await session.save();
 
-  // STEP 6: Return success message.
+  // STEP 7: Return success message.
   return 'Successfully signed up.';
 }
