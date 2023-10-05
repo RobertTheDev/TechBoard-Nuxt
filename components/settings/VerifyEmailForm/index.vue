42f7ad9254a3811c4ea4876e77b6914ef4168989
RobertTheDev <robertthedev@gmail.com>
2023-10-05 23:40:52 +0100

    feat: added get profile fetch handler

--- components/settings/VerifyEmailForm/index.vue
+++ components/settings/VerifyEmailForm/index.vue
@@ -1,17 +1,21 @@
 <template>
-  <button
-    v-if="!formHandler.successMessage"
-    @click="handleSendEmailVerification"
-    :disabled="formHandler.pending"
-  >
-    {{ formHandler.pending ? 'Loading...' : 'Send Email Verification' }}
-  </button>
-  <p v-if="formHandler.successMessage">{{ formHandler.successMessage }}</p>
-  <p v-if="formHandler.errorMessage">{{ formHandler.errorMessage }}</p>
+  <div v-if="profile && !profile.emailVerified">
+    <button
+      v-if="!formHandler.successMessage"
+      @click="handleSendEmailVerification"
+      :disabled="formHandler.pending"
+    >
+      {{ formHandler.pending ? 'Loading...' : 'Send Email Verification' }}
+    </button>
+    <p v-if="formHandler.successMessage">{{ formHandler.successMessage }}</p>
+    <p v-if="formHandler.errorMessage">{{ formHandler.errorMessage }}</p>
+  </div>
+  <p v-if="profile && profile.emailVerified">Email Verified</p>
 </template>
 
 <script lang="ts" setup>
 import IFormHandler from '~/models/configs/interfaces/FormHandler';
+import IProfile from '~/models/profile/interfaces/Profile';
 
 const formHandler = ref<IFormHandler>({
   pending: false,
@@ -35,4 +39,11 @@ async function handleSendEmailVerification(body: any) {
       'Successfully sent email verification token.';
   }
 }
+
+const {
+  data: profile,
+  pending,
+  error,
+  refresh,
+} = await useFetch<IProfile>('/api/profile');
 </script>
