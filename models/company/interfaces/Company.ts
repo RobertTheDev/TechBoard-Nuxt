06305af9d2dc8607ef859b45f4f5653218adec5e
RobertTheDev <robertthedev@gmail.com>
2023-10-06 18:48:50 +0100

    feat: added logo and cover image interfaces

--- models/company/interfaces/Company.ts
+++ models/company/interfaces/Company.ts
@@ -1,6 +1,31 @@
+import ICompanyOwner from '~/models/companyOwner/interfaces/CompanyOwner';
+
+interface ICompanyLogo {
+  // Custom fields.
+  alt: string;
+  url: string;
+}
+
+interface ICoverImage {
+  // Custom fields.
+  alt: string;
+  url: string;
+}
+
 export default interface ICompany {
+  // Default fields.
   _id: string;
   createdAt: string;
   updatedAt?: Date;
+
+  // Custom fields.
+  category: string;
+  coverImage: ICoverImage | null;
+  description: string;
+  logo: ICompanyLogo | null;
   name: string;
+  totalEmployees: string;
+
+  // Lookup fields.
+  owners: ICompanyOwner[];
 }
