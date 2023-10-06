db1d682bae7f465c74d8d96278cb92c0ad6bd1fb
RobertTheDev <robertthedev@gmail.com>
2023-10-06 03:17:33 +0100

    feat: added company admin links to menu

--- components/company/CompanyAdminMenu/index.vue
+++ components/company/CompanyAdminMenu/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h1>Company Admin Menu</h1>
+    <NuxtLink
+      v-for="companyAdminLink in companyAdminLinks"
+      :key="companyAdminLink.path"
+      :to="companyAdminLink.path"
+    >
+      {{ companyAdminLink.name }}
+    </NuxtLink>
   </div>
 </template>
+
+<script setup lang="ts">
+import companyAdminLinks from '~/lib/links/companyAdminLinks';
+</script>
