6092454bdad01fad47c8d8d864a0fff68d50269b
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:08 +0100

    feat: aded seo title

--- pages/settings/verify-email/[emailVerificationToken].vue
+++ pages/settings/verify-email/[emailVerificationToken].vue
@@ -4,3 +4,17 @@
     <VerifyEmailForm />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: ` | ${companyName}`,
+  ogTitle: ` | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
