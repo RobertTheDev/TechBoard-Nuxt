4a84f1c4dd1698ccee9379053229622407c18dc7
RobertTheDev <robertthedev@gmail.com>
2023-10-06 19:16:55 +0100

    feat: added new fields to validation schema

--- models/company/validators/updateCompany.schema.ts
+++ models/company/validators/updateCompany.schema.ts
@@ -1,13 +1,38 @@
-import { object, z } from 'zod';
+import { object, string, z } from 'zod';
 
 // Zod validation schema defines fields required for updating a company.
 const updateCompanySchema = object({
-  name: z
-    .string({
-      invalid_type_error: 'Company name must be a string.',
-      required_error: 'Company name is required.',
-    })
-    .nonempty('Company name cannot be named.')
+  category: string({
+    invalid_type_error: 'Company category must be a string.',
+  })
+    .nonempty('Company category cannot be empty.')
+    .optional(),
+  coverImageUrl: string({
+    invalid_type_error: 'Company cover image URL must be a string.',
+  })
+    .url('Company cover image URL must be in URL format.')
+    .nonempty('Company cover image URL cannot be empty.')
+    .optional(),
+  description: string({
+    invalid_type_error: 'Company description must be a string.',
+  })
+    .nonempty('Company category cannot be empty.')
+    .optional(),
+  logoUrl: string({
+    invalid_type_error: 'Company logo URL must be a string.',
+  })
+    .url('Company logo URL must be in URL format.')
+    .nonempty('Company logo URL cannot be empty.')
+    .optional(),
+  name: string({
+    invalid_type_error: 'Company name must be a string.',
+  })
+    .nonempty('Company name cannot be empty.')
+    .optional(),
+  totalEmployees: string({
+    invalid_type_error: 'Company employees must be a string.',
+  })
+    .nonempty('Company employees cannot be empty.')
     .optional(),
 });
 
