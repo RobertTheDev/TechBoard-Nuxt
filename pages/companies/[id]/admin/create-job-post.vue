2f1cae5e1002d4da72fefa43e0a74f37ffcf9a52
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:39:20 +0100

    feat: aded seo title

--- pages/companies/[id]/admin/create-job-post.vue
+++ pages/companies/[id]/admin/create-job-post.vue
@@ -4,3 +4,17 @@
     <CreateJobPostForm />
   </div>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Create Job Post | ${companyName}`,
+  ogTitle: `Create Job Post | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
