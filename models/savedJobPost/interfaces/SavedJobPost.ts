0cb00a715e15927b96072d6e03f03d36d13aa3b3
RobertTheDev <robertthedev@gmail.com>
2023-10-06 19:16:17 +0100

    feat: added new fields to interface

--- models/savedJobPost/interfaces/SavedJobPost.ts
+++ models/savedJobPost/interfaces/SavedJobPost.ts
@@ -1,5 +1,13 @@
+import IJobPost from '~/models/jobPost/interfaces/JobPost';
+import IUser from '~/models/user/interfaces/User';
+
 export default interface ISavedJobPost {
   _id: string;
   createdAt: string;
   updatedAt?: Date;
+
+  jobPost?: IJobPost;
+  jobPostId: string;
+  user?: IUser;
+  userId: string;
 }
