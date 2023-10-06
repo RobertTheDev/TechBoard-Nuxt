d33045f92507ec5a19a77a3ba5acc181d733ef35
RobertTheDev <robertthedev@gmail.com>
2023-10-06 17:00:28 +0100

    feat: created profile menu component

---
+++ components/profile/ProfileMenu/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <Teleport to="body">
+    <div class="bg-red-300 top-16 absolute right-0 w-auto h-auto p-8">
+      <p>Profile Menu</p>
+      <NuxtLink
+        v-for="profileMenuLink in profileMenuLinks"
+        :to="profileMenuLink.path"
+        :key="profileMenuLink.path"
+        >{{ profileMenuLink.name }}</NuxtLink
+      >
+      <button @click="handleSignOut">Sign Out</button>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import profileMenuLinks from '~/lib/links/profileMenuLinks';
+
+async function handleSignOut() {
+  const { pending, error } = await useFetch(`/api/auth/sign-out`);
+  if (pending.value) {
+    console.log('loading');
+  } else if (error.value) {
+    console.log(error.value);
+  } else {
+    window.location.reload();
+  }
+}
+</script>
