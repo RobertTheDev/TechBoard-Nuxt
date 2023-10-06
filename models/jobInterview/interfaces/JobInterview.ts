98aa4e80505a66542552b871a11bc2a33f428929
RobertTheDev <robertthedev@gmail.com>
2023-10-06 19:16:29 +0100

    feat: added new fields to interface

--- models/jobInterview/interfaces/JobInterview.ts
+++ models/jobInterview/interfaces/JobInterview.ts
@@ -1,5 +1,19 @@
+import IJobPost from '~/models/jobPost/interfaces/JobPost';
+import IUser from '~/models/user/interfaces/User';
+
 export default interface IJobInterview {
+  // Default fields.
   _id: string;
   createdAt: string;
   updatedAt?: Date;
+
+  // Local Fields.
+  startTime: Date;
+  endTime: Date | null;
+
+  // Lookup Fields.
+  jobPost: IJobPost;
+  jobPostId: string;
+  user: IUser;
+  userId: string;
 }
