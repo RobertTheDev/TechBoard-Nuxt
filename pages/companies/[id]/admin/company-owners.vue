824cdda4d4602f9104eb6edc1e836d7f94c4dff9
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:39:59 +0100

    feat: aded seo title

--- pages/companies/[id]/admin/company-owners.vue
+++ pages/companies/[id]/admin/company-owners.vue
@@ -33,4 +33,16 @@ const {
   error,
   refresh,
 } = await useFetch<ICompanyOwner[]>('/api/');
+
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Company Owners | ${companyName}`,
+  ogTitle: `Company Owners | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
 </script>
