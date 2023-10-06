9131500871e67727412a0fbdd1dd54bc813a5e4e
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:38:42 +0100

    feat: aded seo title

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -9,4 +9,16 @@
 definePageMeta({
   middleware: ['is-signed-out'],
 });
+
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Sign In | ${companyName}`,
+  ogTitle: `Sign In | ${companyName}`,
+  description: `Sign in to ${companyName} today to see your job posts and companies and get quick access to the job posts you have saved and companies you follow.`,
+  ogDescription: `Sign in to ${companyName} today to see your job posts and companies and get quick access to the job posts you have saved and companies you follow.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
 </script>
