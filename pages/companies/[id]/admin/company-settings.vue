5aa1e04281ca1e951da3a1e9594b988cfefe53db
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:39:22 +0100

    feat: aded seo title

---
+++ pages/companies/[id]/admin/company-settings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Update Company</h1>
+    <UpdateCompanyForm />
+  </div>
+</template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Company Settings | ${companyName}`,
+  ogTitle: `Company Settings | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
