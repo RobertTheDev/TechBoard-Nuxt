18f4304c69e9d910dd8a0aa53a7d707e1cb34768
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:39:54 +0100

    feat: aded seo title

--- pages/companies/create-company.vue
+++ pages/companies/create-company.vue
@@ -4,3 +4,17 @@
     <CreateCompanyForm />
   </div>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Create Company | ${companyName}`,
+  ogTitle: `Create Company | ${companyName}`,
+  description: `Ready to find candidates for your tech companies? Create your company on ${companyName} today! Our platform makes it easy to find candidates for your company, find top talent, and share your job posts with the world.`,
+  ogDescription: `Ready to find candidates for your tech companies? Create your company on ${companyName} today! Our platform makes it easy to find candidates for your company, find top talent, and share your job posts with the world.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
