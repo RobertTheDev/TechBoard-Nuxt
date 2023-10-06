3056aaefca5ad56edbb485f5fa6e496934729e1d
RobertTheDev <robertthedev@gmail.com>
2023-10-06 22:45:53 +0100

    feat: created server handler route

---
+++ server/api/profile/saved-jobs/index.ts
@@ -0,0 +1,20 @@
+import getSavedJobPostsByProfile from '~/server/handlers/profile/savedJobPosts';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  if (method === 'GET') {
+    try {
+      return await getSavedJobPostsByProfile(event);
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+      );
+
+      return error;
+    }
+  }
+});
