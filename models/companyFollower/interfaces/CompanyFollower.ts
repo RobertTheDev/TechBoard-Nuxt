ca9bb936bfd0217136d25bcbeb2e2857282acd7b
RobertTheDev <robertthedev@gmail.com>
2023-10-06 19:16:42 +0100

    feat: added new fields to interface

--- models/companyFollower/interfaces/CompanyFollower.ts
+++ models/companyFollower/interfaces/CompanyFollower.ts
@@ -1,5 +1,13 @@
+import ICompany from '~/models/company/interfaces/Company';
+import IUser from '~/models/user/interfaces/User';
+
 export default interface ICompanyFollower {
   _id: string;
   createdAt: string;
   updatedAt?: Date;
+
+  company?: ICompany | null;
+  companyId: string;
+  user?: IUser | null;
+  userId: string;
 }
