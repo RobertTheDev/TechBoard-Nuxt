6a86ee7b63ecbb799afbcc9d5d96a0e16c89d759
RobertTheDev <robertthedev@gmail.com>
2023-10-06 02:45:58 +0100

    feat: added use fetch functionality

--- pages/job-interviews/[id]/index.vue
+++ pages/job-interviews/[id]/index.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
     <h1>Job Interview</h1>
+
+    <div v-if="jobInterview">
+      <p>{{ jobInterview._id }}</p>
+    </div>
+
+    <div v-if="!jobInterview">
+      <p>No job interview was found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import IJobInterview from '@/models/jobInterview/interfaces/JobInterview';
+
+const {
+  data: jobInterview,
+  pending,
+  error,
+  refresh,
+} = await useFetch<IJobInterview | null>('/api/job-interviews/:id');
+</script>
