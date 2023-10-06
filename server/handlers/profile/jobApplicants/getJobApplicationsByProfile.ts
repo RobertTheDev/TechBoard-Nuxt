48a1659e8ad49db7b92c4cf2b0e0811a8a9053b3
RobertTheDev <robertthedev@gmail.com>
2023-10-06 22:45:31 +0100

    feat: created handler

---
+++ server/handlers/profile/jobApplicants/getJobApplicationsByProfile.ts
@@ -0,0 +1,40 @@
+import { jobApplicationsCollection } from '~/server/lib/db/mongodb/collections';
+import isAuthenticated from '../../auth/isAuthenticated';
+import { H3Event, EventHandlerRequest } from 'h3';
+import getSessionUser from '../../auth/getSessionUser';
+import { ObjectId } from 'mongodb';
+
+// This handler finds and returns all the job applications to the signed in user from the db.
+
+export default async function getJobApplicationsByProfile(
+  event: H3Event<EventHandlerRequest>,
+) {
+  // STEP 1: Check user is authenticated.
+  await isAuthenticated(event);
+
+  // STEP 2: Get the user id.
+  const { _id: userId } = await getSessionUser(event);
+
+  // STEP 3: Find job applications by user id.
+  return await jobApplicationsCollection
+    .aggregate(
+      [
+        {
+          $match: {
+            userId: new ObjectId(userId),
+          },
+        },
+        {
+          $lookup: {
+            from: 'jobPosts',
+            localField: 'jobPostId',
+            foreignField: '_id',
+            as: 'jobPost',
+          },
+        },
+        { $unwind: { path: '$jobPost' } },
+      ],
+      { maxTimeMS: 60000, allowDiskUse: true },
+    )
+    .toArray();
+}
