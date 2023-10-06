15d1b462014f71139859c977139e0b8313e0fd5e
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:05 +0100

    feat: aded seo title

--- pages/settings/verify-email/index.vue
+++ pages/settings/verify-email/index.vue
@@ -4,3 +4,17 @@
     <SendEmailVerficationForm />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Verify Email | ${companyName}`,
+  ogTitle: `Verify Email | ${companyName}`,
+  description: `Verify your email address on ${companyName} by clicking the verify email button and we will send an email to your account's email address with the verification steps.`,
+  ogDescription: `Verify your email address on ${companyName} by clicking the verify email button and we will send an email to your account's email address with the verification steps.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
