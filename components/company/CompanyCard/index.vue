530c9870be04dd81cd317ea12dce4f946c0cabf5
RobertTheDev <robertthedev@gmail.com>
2023-10-06 18:48:32 +0100

    style: added form styles

--- components/company/CompanyCard/index.vue
+++ components/company/CompanyCard/index.vue
@@ -1,11 +1,54 @@
 <template>
-  <NuxtLink :to="`/companies/${company._id}`">
+  <div>
+    <p>{{ company.category }}</p>
+    <p>{{ company.createdAt }}</p>
+    <p>{{ company.description }}</p>
     <p>{{ company.name }}</p>
-  </NuxtLink>
+    <p>{{ company.totalEmployees }}</p>
+
+    <div v-if="company.coverImage">
+      <img :src="company.coverImage.url" :alt="company.coverImage.alt" />
+    </div>
+
+    <div v-if="company.logo">
+      <img :src="company.logo.url" :alt="company.logo.alt" />
+    </div>
+
+    <button @click="navigateToCompany(company._id)">View Company</button>
+
+    <button @click="followCompany(company._id)">
+      {{ followPending ? 'Loading...' : 'Follow' }}
+    </button>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import ICompany from '~/models/company/interfaces/Company';
 
 const company = defineProps<ICompany>();
+
+const router = useRouter();
+
+function navigateToCompany(companyId: string) {
+  router.push(`/companies/${companyId}`);
+}
+
+const followPending = ref(false);
+
+async function followCompany(companyId: string) {
+  const { pending, error, data } = await useFetch(
+    `/api/company/${companyId}/follow`,
+    {
+      method: 'POST',
+    },
+  );
+
+  if (pending) {
+    followPending.value = true;
+  }
+
+  if (data || error) {
+    followPending.value = false;
+  }
+}
 </script>
