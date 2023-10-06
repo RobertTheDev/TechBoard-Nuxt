3d097271705f4082f7232714821bfbe0a6126088
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:42 +0100

    feat: aded seo title

--- pages/settings/change-password.vue
+++ pages/settings/change-password.vue
@@ -4,3 +4,17 @@
     <ChangePasswordForm />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Change Password | ${companyName}`,
+  ogTitle: `Change Password | ${companyName}`,
+  description: `To change your password for your account on ${companyName} - enter your new password followed by your current password.`,
+  ogDescription: `To change your password for your account on ${companyName} - enter your new password followed by your current password.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
