490b42b6a6757ac066334cff1334ee24615e1df9
RobertTheDev <robertthedev@gmail.com>
2023-10-06 03:17:21 +0100

    feat: created company admin links

---
+++ lib/links/companyAdminLinks.ts
@@ -0,0 +1,32 @@
+const companyAdminLinks = [
+  {
+    name: 'Company Owners',
+    path: '/admin/company-owners',
+  },
+  {
+    name: 'List A Job',
+    path: '/admin/create-job-post',
+  },
+  {
+    name: 'Job Applications',
+    path: '/admin/job-applications',
+  },
+  {
+    name: 'Job Interviews',
+    path: '/admin/job-interviews',
+  },
+  {
+    name: 'Job Offers',
+    path: '/admin/job-offers',
+  },
+  {
+    name: 'Listed Job Posts',
+    path: '/admin/job-posts',
+  },
+  {
+    name: 'Company Settings',
+    path: '/admin/company-settings',
+  },
+];
+
+export default companyAdminLinks;
