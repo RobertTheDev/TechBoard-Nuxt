d65d2262ec1a692436bd737eaf49817d57b5cda0
RobertTheDev <robertthedev@gmail.com>
2023-10-06 01:12:06 +0100

    feat: defined job interview component path

--- components.config.ts
+++ components.config.ts
@@ -12,6 +12,10 @@ const componentsConfig = [
     pathPrefix: false,
   },
   {
+    path: '~/components/jobInterview',
+    pathPrefix: false,
+  },
+  {
     path: '~/components/jobOffer',
     pathPrefix: false,
   },
