4658566ae8f0cf1da94a87b8985c918f22f284e8
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:15 +0100

    feat: aded seo title

--- pages/profile/job-interviews.vue
+++ pages/profile/job-interviews.vue
@@ -33,4 +33,16 @@ const {
   error,
   refresh,
 } = await useFetch<IJobInterview[]>('/api/');
+
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Job Interviews | ${companyName}`,
+  ogTitle: `Job Interviews | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
 </script>
