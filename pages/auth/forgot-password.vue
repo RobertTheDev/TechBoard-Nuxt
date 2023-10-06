49c0324e806fb8c8bdde7dae1b4316e6d60820f3
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:38:39 +0100

    feat: aded seo title

--- pages/auth/forgot-password.vue
+++ pages/auth/forgot-password.vue
@@ -9,4 +9,16 @@
 definePageMeta({
   middleware: ['is-signed-out'],
 });
+
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Forgot Password | ${companyName}`,
+  ogTitle: `Forgot Password | ${companyName}`,
+  description: `If you have forgotten your password to your ${companyName} account - enter your email address below and we will send you an email with a link to get back in.`,
+  ogDescription: `If you have forgotten your password to your ${companyName} account - enter your email address below and we will send you an email with a link to get back in.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
 </script>
