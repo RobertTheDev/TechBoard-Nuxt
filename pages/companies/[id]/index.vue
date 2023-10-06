6b1f895262e4a36e7f7848b0b977bb41bec88965
RobertTheDev <robertthedev@gmail.com>
2023-10-06 02:44:23 +0100

    feat: added use fetch functionality

--- pages/companies/[id]/index.vue
+++ pages/companies/[id]/index.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
     <h1>Company</h1>
+
+    <div v-if="company">
+      <p>{{ company.name }}</p>
+    </div>
+
+    <div v-if="!company">
+      <p>No company was found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ICompany from '@/models/company/interfaces/Company';
+
+const {
+  data: company,
+  pending,
+  error,
+  refresh,
+} = await useFetch<ICompany | null>('/api/companies/:id');
+</script>
