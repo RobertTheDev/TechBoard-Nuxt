910b253cc8bdc640a9a832a903c67899bff97bf7
RobertTheDev <robertthedev@gmail.com>
2023-10-06 02:45:37 +0100

    feat: added use fetch functionality

--- pages/companies/[id]/admin/job-posts.vue
+++ pages/companies/[id]/admin/job-posts.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    <h1>Job Posts</h1>
+    <div v-if="jobPosts">
+      <JobPostCard
+        v-for="jobPost in jobPosts"
+        :key="jobPost._id"
+        v-bind="jobPost"
+      />
+    </div>
+
+    <div v-if="!jobPosts">
+      <p>No job posts found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import IJobPost from '@/models/jobPost/interfaces/JobPost';
+
+const {
+  data: jobPosts,
+  pending,
+  error,
+  refresh,
+} = await useFetch<IJobPost[]>('/api/job-posts');
+</script>
