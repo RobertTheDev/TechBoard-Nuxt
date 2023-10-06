b2390fd50026a7be70442b2400061f831314f2d8
RobertTheDev <robertthedev@gmail.com>
2023-10-06 02:44:05 +0100

    feat: added company car component

--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
     <h1>Companies</h1>
+
+    <div v-if="companies">
+      <CompanyCard
+        v-for="company in companies"
+        :key="company._id"
+        v-bind="company"
+      />
+    </div>
+
+    <div v-if="!companies">
+      <p>No companies were found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ICompany from '@/models/company/interfaces/Company';
+
+const {
+  data: companies,
+  pending,
+  error,
+  refresh,
+} = await useFetch<ICompany[]>('/api/companies');
+</script>
