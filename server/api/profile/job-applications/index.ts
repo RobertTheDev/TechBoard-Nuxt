3f666e2081f74f0dff6257b4a0350269bc5144bb
RobertTheDev <robertthedev@gmail.com>
2023-10-06 22:46:06 +0100

    feat: created server handler route

---
+++ server/api/profile/job-applications/index.ts
@@ -0,0 +1,20 @@
+import getJobApplicationsByProfile from '~/server/handlers/profile/jobApplicants/getJobApplicationsByProfile';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  if (method === 'GET') {
+    try {
+      return await getJobApplicationsByProfile(event);
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+      );
+
+      return error;
+    }
+  }
+});
