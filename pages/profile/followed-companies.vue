54a07f1ec479ca81c1b6da49b40a686522969c4d
RobertTheDev <robertthedev@gmail.com>
2023-10-06 22:46:30 +0100

    feat: added use fetch api handler

---
+++ pages/profile/followed-companies.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Followed Companies</h1>
+
+    <div v-if="followedCompanies">
+      <p
+        v-for="followedCompany in followedCompanies"
+        :key="followedCompany._id"
+        v-bind="followedCompany"
+      >
+        {{ followedCompany.company?._id }}
+      </p>
+    </div>
+
+    <div v-if="!followedCompanies || followedCompanies.length <= 0">
+      <p>No followed companies were found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ICompanyFollower from '~/models/companyFollower/interfaces/CompanyFollower';
+
+const {
+  data: followedCompanies,
+  pending,
+  error,
+  refresh,
+} = await useFetch<ICompanyFollower[]>('/api/profile/followed-companies');
+
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Followed Companies | ${companyName}`,
+  ogTitle: `Followed Companies | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
