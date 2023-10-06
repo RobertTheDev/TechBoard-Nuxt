ab9c4709115e02c5aab3936b6d3f1571ba8d9c64
RobertTheDev <robertthedev@gmail.com>
2023-10-06 02:44:55 +0100

    feat: added use fetch functionality

--- pages/companies/[id]/admin/company-owners.vue
+++ pages/companies/[id]/admin/company-owners.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <h1>Company Owners</h1>
-    <button>Create Company Owner</button>
+
+    <div v-if="companyOwners">
+      <CompanyOwnerCard
+        v-for="companyOwner in companyOwners"
+        :key="companyOwner._id"
+        v-bind="companyOwner"
+      />
+    </div>
+
+    <div v-if="!companyOwners">
+      <p>No company owners were found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ICompanyOwner from '@/models/companyOwner/interfaces/CompanyOwner';
+
+const {
+  data: companyOwners,
+  pending,
+  error,
+  refresh,
+} = await useFetch<ICompanyOwner[]>('/api/');
+</script>
