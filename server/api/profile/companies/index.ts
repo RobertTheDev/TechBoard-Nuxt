ebf7706de74e0111dbfa5dd05ab86ae51bef0d71
RobertTheDev <robertthedev@gmail.com>
2023-10-06 22:46:13 +0100

    feat: created server handler route

---
+++ server/api/profile/companies/index.ts
@@ -0,0 +1,20 @@
+import getCompaniesByProfile from '~/server/handlers/profile/companies/getCompaniesByProfile';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  if (method === 'GET') {
+    try {
+      return await getCompaniesByProfile(event);
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+      );
+
+      return error;
+    }
+  }
+});
