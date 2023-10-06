e818ba39c289090efc905fe0133c0266c126f7cc
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:32 +0100

    feat: aded seo title

--- pages/job-posts/[id]/update.vue
+++ pages/job-posts/[id]/update.vue
@@ -4,3 +4,17 @@
     <UpdateJobPostForm />
   </div>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Update Job Post | ${companyName}`,
+  ogTitle: `Update Job Post | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
