1854ce3afedf6540d82af3ca45fe356712304212
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:23 +0100

    feat: aded seo title

--- pages/settings/change-email.vue
+++ pages/settings/change-email.vue
@@ -4,3 +4,17 @@
     <ChangeEmailForm />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Change Email | ${companyName}`,
+  ogTitle: `Change Email | ${companyName}`,
+  description: `Change your email on ${companyName} by entering a new email address in the input below and authenticate with your password.`,
+  ogDescription: `Change your email on ${companyName} by entering a new email address in the input below and authenticate with your password.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
