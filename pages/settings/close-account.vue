6aec98297a245ac8b4e560a7be009a507482c0ad
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:11 +0100

    feat: aded seo title

--- pages/settings/close-account.vue
+++ pages/settings/close-account.vue
@@ -4,3 +4,17 @@
     <CloseAccountForm />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Close Account | ${companyName}`,
+  ogTitle: `Close Account | ${companyName}`,
+  description: `To close and delete your account on ${companyName} - and remove all associated data. Enter your password and then type 'DELETE'.`,
+  ogDescription: `To close and delete your account on ${companyName} - and remove all associated data. Enter your password and then type 'DELETE'.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
