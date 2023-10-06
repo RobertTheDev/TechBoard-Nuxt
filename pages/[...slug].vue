b71415c17165ceb56f4c9a9cda65f584c8b840b5
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:38:36 +0100

    feat: aded seo title

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -7,3 +7,17 @@
     </ContentDoc>
   </main>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: ` | ${companyName}`,
+  ogTitle: ` | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
