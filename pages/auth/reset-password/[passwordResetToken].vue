9c9a509b8db25a8a52274ee16f93026b1cec73c3
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:38:45 +0100

    feat: aded seo title

--- pages/auth/reset-password/[passwordResetToken].vue
+++ pages/auth/reset-password/[passwordResetToken].vue
@@ -9,4 +9,16 @@
 definePageMeta({
   middleware: ['is-signed-out'],
 });
+
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Reset Password | ${companyName}`,
+  ogTitle: `Reset Password | ${companyName}`,
+  description: `Complete the form to reset your ${companyName} password. Your password must be at least eight characters long containing a letter, number and special character.`,
+  ogDescription: `Complete the form to reset your ${companyName} password. Your password must be at least eight characters long containing a letter, number and special character.`,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
 </script>
