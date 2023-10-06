3f625ec5d0c25cd2434d9682ed5c4ddfe927de8f
RobertTheDev <robertthedev@gmail.com>
2023-10-06 01:39:17 +0100

    feat: created component

---
+++ components/jobApplication/JobApplicationCard/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>{{ jobApplication._id }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IJobApplication from '~/models/jobApplication/interfaces/JobApplication';
+
+const jobApplication = defineProps<IJobApplication>();
+</script>
