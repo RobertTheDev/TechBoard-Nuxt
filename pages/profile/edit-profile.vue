ad3c074ced2886203ff576b93c52d897d4128931
RobertTheDev <robertthedev@gmail.com>
2023-10-06 16:40:27 +0100

    feat: aded seo title

--- pages/profile/edit-profile.vue
+++ pages/profile/edit-profile.vue
@@ -4,3 +4,17 @@
     <EditProfileForm />
   </div>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Edit Profile | ${companyName}`,
+  ogTitle: `Edit Profile | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
