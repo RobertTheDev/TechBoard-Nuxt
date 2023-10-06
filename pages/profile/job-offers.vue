7ef74f4eaa39e1d476deca2e39f70d577763331c
RobertTheDev <robertthedev@gmail.com>
2023-10-06 02:47:07 +0100

    feat: added use fetch functionality

--- pages/profile/job-offers.vue
+++ pages/profile/job-offers.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
     <h1>Job Offers</h1>
+
+    <div v-if="jobOffers">
+      <JobOfferCard
+        v-for="jobOffer in jobOffers"
+        :key="jobOffer._id"
+        v-bind="jobOffer"
+      />
+    </div>
+
+    <div v-if="!jobOffers">
+      <p>No job offers were found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import IJobOffer from '~/models/jobOffer/interfaces/JobOffer';
+
+const {
+  data: jobOffers,
+  pending,
+  error,
+  refresh,
+} = await useFetch<IJobOffer[]>('/api/');
+</script>
