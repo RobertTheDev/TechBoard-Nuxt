d12457a557cff1f0e5396a3f1b4f8fe09f9f8901
RobertTheDev <robertthedev@gmail.com>
2023-10-06 02:47:25 +0100

    feat: added use fetch functionality

--- pages/profile/saved-jobs.vue
+++ pages/profile/saved-jobs.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
     <h1>Saved Jobs</h1>
+
+    <div v-if="savedJobPosts">
+      <JobOfferCard
+        v-for="savedJobPost in savedJobPosts"
+        :key="savedJobPost._id"
+        v-bind="savedJobPost"
+      />
+    </div>
+
+    <div v-if="!savedJobPosts">
+      <p>No saved jobs were found.</p>
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ISavedJobPost from '~/models/savedJobPost/interfaces/SavedJobPost';
+
+const {
+  data: savedJobPosts,
+  pending,
+  error,
+  refresh,
+} = await useFetch<ISavedJobPost[]>('/api/');
+</script>
