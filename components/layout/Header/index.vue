0985e0abd12c0e3ab84e2c07b1b7304b476045ca
RobertTheDev <robertthedev@gmail.com>
2023-10-06 17:00:37 +0100

    feat: added profile menu component

--- components/layout/Header/index.vue
+++ components/layout/Header/index.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="bg-white h-16 min-w-full flex items-center p-4 shadow-lg">
-    <div class="flex flex-1 justify-start items-center">
+    <div class="flex justify-start items-center">
       <NuxtLink to="/">{{ companyName }}</NuxtLink>
     </div>
-    <div>
+    <div class="flex items-center justify-center flex-1 gap-8">
       <NuxtLink
         v-for="navigationLink in navigationLinks"
         :to="navigationLink.path"
@@ -11,30 +11,28 @@
         >{{ navigationLink.name }}</NuxtLink
       >
     </div>
-    <div class="flex flex-1 justify-end items-center">
+    <div class="flex justify-end items-center">
       <div v-if="!profile">
         <NuxtLink to="/auth/sign-in">Sign In</NuxtLink>
       </div>
-      <div v-if="profile">
-        <p>{{ profile.emailAddress }}</p>
+      <div class="flex items-center gap-4" v-if="profile">
         <NuxtLink to="/job-posts/create">Create Job Post</NuxtLink>
 
-        <button @click="handleToggleProfileMenu">Profile</button>
-        <Teleport to="body">
-          <div
-            v-if="profileMenuActive"
-            class="bg-red-300 top-16 absolute right-0 w-auto h-auto p-8"
-          >
-            <p>Profile Menu</p>
-            <NuxtLink
-              v-for="profileMenuLink in profileMenuLinks"
-              :to="profileMenuLink.path"
-              :key="profileMenuLink.path"
-              >{{ profileMenuLink.name }}</NuxtLink
-            >
-            <button @click="handleSignOut">Sign Out</button>
-          </div>
-        </Teleport>
+        <div class="h-10 w-10 rounded-full overflow-hidden">
+          <input
+            type="image"
+            @click="handleToggleProfileMenu"
+            class="h-full w-full object-cover"
+            :src="
+              profile.avatar
+                ? profile.avatar
+                : 'https://i.stack.imgur.com/l60Hf.png'
+            "
+            alt="Profile Image"
+          />
+        </div>
+
+        <ProfileMenu v-if="profileMenuActive" />
       </div>
     </div>
   </header>
@@ -42,8 +40,8 @@
 
 <script setup lang="ts">
 import companyName from '~/lib/constants/companyName';
-import profileMenuLinks from '~/lib/links/profileMenuLinks';
 import navigationLinks from '~/lib/links/navigationLinks';
+import IProfile from '~/models/profile/interfaces/Profile';
 
 const profileMenuActive = ref(false);
 
@@ -51,21 +49,10 @@ function handleToggleProfileMenu() {
   profileMenuActive.value = !profileMenuActive.value;
 }
 
-async function handleSignOut() {
-  const { pending, error } = await useFetch(`/api/auth/sign-out`);
-  if (pending.value) {
-    console.log('loading');
-  } else if (error.value) {
-    console.log(error.value);
-  } else {
-    window.location.reload();
-  }
-}
-
 const {
   data: profile,
   pending,
   error,
   refresh,
-} = await useFetch<{ id: string; emailAddress: string }>('/api/profile');
+} = await useFetch<IProfile>('/api/profile');
 </script>
