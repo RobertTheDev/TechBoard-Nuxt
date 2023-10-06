55300bdc00094bcac336c3af586394509b07adc9
RobertTheDev <robertthedev@gmail.com>
2023-10-06 19:16:21 +0100

    feat: added new fields to interface

--- models/jobPost/interfaces/JobPost.ts
+++ models/jobPost/interfaces/JobPost.ts
@@ -1,6 +1,30 @@
+import ICompany from '~/models/company/interfaces/Company';
+
+interface ISalary {
+  min: number;
+  max: number;
+  rate: string;
+}
+
 export default interface IJobPost {
+  // Default fields.
   _id: string;
   createdAt: Date;
   updatedAt?: Date;
+
+  // Custom fields.
+  company: ICompany;
+  companyId: string;
+  contractType: string;
+  deadlineDate: Date;
+  description: string;
+  location?: {
+    address: string;
+  };
+  locationType: string;
+  published: boolean;
+  salary: ISalary;
+  seniorLevel: string;
   title: string;
+  totalApplicants: number;
 }
