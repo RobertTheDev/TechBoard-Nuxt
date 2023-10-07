0e4309ef6bcac46e5aad1a384449c1c7a1fb751a
RobertTheDev <robertthedev@gmail.com>
2023-10-07 01:59:02 +0100

    feat: created page

---
+++ pages/companies/[id]/admin/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLayout name="company-admin">
+    <div>
+      <p>Admin</p>
+      <div class="flex flex-col">
+        <NuxtLink
+          v-for="companyAdminLink in companyAdminLinks"
+          :to="companyAdminLink.path"
+        >
+          {{ companyAdminLink.name }}
+        </NuxtLink>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import companyAdminLinks from '~/lib/links/companyAdminLinks';
+</script>
