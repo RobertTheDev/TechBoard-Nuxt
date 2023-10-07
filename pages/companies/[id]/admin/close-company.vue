186b7a424935b51647a2f10b6cb00ddc28b8e2db
RobertTheDev <robertthedev@gmail.com>
2023-10-07 17:43:44 +0100

    feat: created close company page

---
+++ pages/companies/[id]/admin/close-company.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLayout name="company-admin">
+    <div>
+      <h1>Close Company</h1>
+      <CloseCompanyForm />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Close Company | ${companyName}`,
+  ogTitle: `Close Company | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
