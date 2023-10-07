ef0b8b33f4fdb82ad559fb1635af716c723d1cd1
RobertTheDev <robertthedev@gmail.com>
2023-10-07 17:20:29 +0100

    feat: defined job post admin links

---
+++ lib/links/jobPostAdminLinks.ts
@@ -0,0 +1,25 @@
+const jobPostAdminLinks = [
+  {
+    name: 'Update Job Post',
+    path: 'create-job-post',
+  },
+  {
+    name: 'View Job Post',
+    path: 'job-post',
+  },
+  {
+    name: 'Job Interviews',
+    path: 'job-interviews',
+  },
+
+  {
+    name: 'Job Offers',
+    path: 'job-offers',
+  },
+  {
+    name: 'Company Owners',
+    path: 'company-owners',
+  },
+];
+
+export default jobPostAdminLinks;
