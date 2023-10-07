fa4c5c53ef54ea31531ae1f3f6b0b8331aeda096
RobertTheDev <robertthedev@gmail.com>
2023-10-07 20:41:33 +0100

    fix: fixed admin link

--- pages/companies/[id]/admin/index.vue
+++ pages/companies/[id]/admin/index.vue
@@ -5,7 +5,8 @@
       <div class="flex flex-col">
         <NuxtLink
           v-for="companyAdminLink in companyAdminLinks"
-          :to="companyAdminLink.path"
+          :key="companyAdminLink.path"
+          :to="`/companies/${companyId}/admin/${companyAdminLink.path}`"
         >
           {{ companyAdminLink.name }}
         </NuxtLink>
@@ -16,4 +17,6 @@
 
 <script setup lang="ts">
 import companyAdminLinks from '~/lib/links/companyAdminLinks';
+
+const { id: companyId } = useRoute().params;
 </script>
