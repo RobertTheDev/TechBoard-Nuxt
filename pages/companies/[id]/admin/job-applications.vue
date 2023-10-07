0091d56bb2cb8e3dfb560c8c639c7cbabc58faf0
RobertTheDev <robertthedev@gmail.com>
2023-10-07 01:57:30 +0100

    feat: added company admin layout

--- pages/companies/[id]/admin/job-applications.vue
+++ pages/companies/[id]/admin/job-applications.vue
@@ -1,27 +1,29 @@
 <template>
-  <div>
-    <h1>Job Applications</h1>
+  <NuxtLayout name="company-admin">
+    <div>
+      <h1>Job Applications</h1>
 
-    <div v-if="jobApplications">
-      <JobApplicationCard
-        v-for="jobApplication in jobApplications"
-        :key="jobApplication._id"
-        v-bind="jobApplication"
-      />
-    </div>
+      <div v-if="jobApplications">
+        <JobApplicationCard
+          v-for="jobApplication in jobApplications"
+          :key="jobApplication._id"
+          v-bind="jobApplication"
+        />
+      </div>
 
-    <div v-if="!jobApplications">
-      <p>No job applications were found.</p>
-    </div>
+      <div v-if="!jobApplications">
+        <p>No job applications were found.</p>
+      </div>
 
-    <div v-if="pending">
-      <p>Loading...</p>
-    </div>
+      <div v-if="pending">
+        <p>Loading...</p>
+      </div>
 
-    <div v-if="error">
-      <button @click="refresh()">Retry</button>
+      <div v-if="error">
+        <button @click="refresh()">Retry</button>
+      </div>
     </div>
-  </div>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
