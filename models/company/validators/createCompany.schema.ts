b5ed652d1140fe74365ad2194936b7dce8358d1e
RobertTheDev <robertthedev@gmail.com>
2023-10-08 22:41:13 +0100

    refactor: changed non empty error message

--- models/company/validators/createCompany.schema.ts
+++ models/company/validators/createCompany.schema.ts
@@ -5,32 +5,32 @@ const createCompanySchema = object({
   category: string({
     required_error: 'A company category is required.',
     invalid_type_error: 'Company category must be a string.',
-  }).nonempty('Company category cannot be empty.'),
+  }).nonempty('Company category is required.'),
   coverImageUrl: string({
     invalid_type_error: 'Company cover image URL must be a string.',
   })
     .url('Company cover image URL must be in URL format.')
-    .nonempty('Company cover image URL cannot be empty.')
+    .nonempty('Company cover image URL is required.')
     .optional(),
   description: string({
     invalid_type_error: 'Company description must be a string.',
   })
-    .nonempty('Company category cannot be empty.')
+    .nonempty('Company category is required.')
     .optional(),
   logoUrl: string({
     invalid_type_error: 'Company logo URL must be a string.',
   })
     .url('Company logo URL must be in URL format.')
-    .nonempty('Company logo URL cannot be empty.')
+    .nonempty('Company logo URL is required.')
     .optional(),
   name: string({
     required_error: 'A company name is required.',
     invalid_type_error: 'Company name must be a string.',
-  }).nonempty('Company name cannot be empty.'),
+  }).nonempty('Company name is required.'),
   totalEmployees: string({
     required_error: 'A company employees is required.',
     invalid_type_error: 'Company employees must be a string.',
-  }).nonempty('Company employees cannot be empty.'),
+  }).nonempty('Company employees is required.'),
 });
 
 // Create a TypeScript type from the schema.
