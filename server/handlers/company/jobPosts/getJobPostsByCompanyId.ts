a010416db8e91dc17ac309d759bfd4f7d4ab4be1
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:58:42 +0100

    feat: created handler

---
+++ server/handlers/company/jobPosts/getJobPostsByCompanyId.ts
@@ -0,0 +1,17 @@
+import { jobPostsCollection } from '~/server/lib/db/mongodb/collections';
+import { H3Event, EventHandlerRequest } from 'h3';
+import { ObjectId } from 'mongodb';
+
+// This handler finds and returns all the job posts by matching company id from the db.
+
+export default async function getJobPostsByCompanyId(
+  event: H3Event<EventHandlerRequest>,
+) {
+  const { id: companyId } = event.context.params as { id: string };
+
+  return await jobPostsCollection
+    .find({
+      companyId: new ObjectId(companyId),
+    })
+    .toArray();
+}
