f674c16c8cf1b8959961fe9b43a1fc83382cb431
RobertTheDev <robertthedev@gmail.com>
2023-10-08 22:40:18 +0100

    feat: created schema

--- models/jobApplication/validators/updateJobApplication.schema.ts
+++ models/jobApplication/validators/updateJobApplication.schema.ts
@@ -1,7 +1,27 @@
-import { object, z } from 'zod';
+import { object, string, z } from 'zod';
+import jobApplicationStatusTypes from '../statusTypes/jobApplicationStatusTypes';
 
 // Zod validation schema defines fields required for updating a job application.
-const updateJobApplicationSchema = object({});
+const updateJobApplicationSchema = object({
+  cvFile: string({
+    invalid_type_error: 'CV must be a string.',
+  })
+    .nonempty('CV is required.')
+    .optional(),
+  message: string({
+    invalid_type_error: 'Message must be a string.',
+  })
+    .nonempty('Message cannot be empty.')
+    .optional(),
+  status: string({
+    invalid_type_error: 'Job application must be a string.',
+  })
+    .refine((value) => jobApplicationStatusTypes.includes(value), {
+      message:
+        'Invalid status. Allowed values are ACCEPTED, REJECTED, or SENT.',
+    })
+    .optional(),
+});
 
 // Create a TypeScript type from the schema.
 export type UpdateJobApplicationSchemaType = z.infer<
