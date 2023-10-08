0b6eebd604e0f0b4523f7502e6d723ee00da9b3c
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:01:38 +0100

    feat: added update company by id

--- server/api/companies/[id]/index.ts
+++ server/api/companies/[id]/index.ts
@@ -1,5 +1,6 @@
 import deleteCompanyById from '~/server/handlers/company/deleteCompanyById';
 import getCompanyById from '~/server/handlers/company/getCompanyById';
+import updateCompanyById from '~/server/handlers/company/updateCompanyById';
 import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
 
 export default defineEventHandler(async (event) => {
@@ -9,6 +10,20 @@ export default defineEventHandler(async (event) => {
   if (method === 'GET') {
     try {
       return await getCompanyById(event);
+    } catch (error) {
+      // If an error occurs then log the error and return an unsuccessful statement.
+      const errorMessage = (error as Error).message;
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+      );
+      return error;
+    }
+  }
+
+  // This handler updates a company owner by its unique id and a validated bpdy in the database.
+  if (method === 'PUT') {
+    try {
+      return await updateCompanyById(event);
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
