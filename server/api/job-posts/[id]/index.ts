69949b48436a121ae7f04349ba114e5e70917f11
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:01:00 +0100

    feat: added update job post handler

--- server/api/job-posts/[id]/index.ts
+++ server/api/job-posts/[id]/index.ts
@@ -1,17 +1,14 @@
 import deleteJobPostById from '~/server/handlers/jobPost/deleteJobPostById';
 import getJobPostById from '~/server/handlers/jobPost/getJobPostById';
-import updateJobPostById from '~/server/handlers/jobPost/updateJobPostById';
 import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
 
 export default defineEventHandler(async (event) => {
   const { method, originalUrl } = event.node.req;
 
-  const { id } = event.context.params as { id: string };
-
   // This handler gets a job post by its unique id from the database.
   if (method === 'GET') {
     try {
-      return await getJobPostById(id);
+      return await getJobPostById(event);
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
@@ -25,23 +22,7 @@ export default defineEventHandler(async (event) => {
   // This handler deletes a job post by its unique id from the database.
   if (method === 'DELETE') {
     try {
-      return await deleteJobPostById(id);
-    } catch (error) {
-      // If an error occurs then log the error and return an unsuccessful statement.
-      const errorMessage = (error as Error).message;
-      winstonLogger.error(
-        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
-      );
-      return error;
-    }
-  }
-
-  // This handler updates a job post by its unique id from the database.
-  if (method === 'PUT') {
-    try {
-      const body = await readBody(event);
-
-      return await updateJobPostById(id, body);
+      return await deleteJobPostById(event);
     } catch (error) {
       // If an error occurs then log the error and return an unsuccessful statement.
       const errorMessage = (error as Error).message;
