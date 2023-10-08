37f0b44399a9f79a3245b856bf157fe7d06ed6e2
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:04:34 +0100

    feat: edit job post page

---
+++ pages/job-posts/[id]/admin/edit-job-post.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLayout name="job-post-admin">
+    <div>
+      <h1>Update Job Post</h1>
+      <UpdateJobPostForm />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Update Job Post | ${companyName}`,
+  ogTitle: `Update Job Post | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
