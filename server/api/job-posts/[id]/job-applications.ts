21ffb631c99f5773907600737ba1c049381502a8
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:00:19 +0100

    feat: created handler route

---
+++ server/api/job-posts/[id]/job-applications.ts
@@ -0,0 +1,20 @@
+import getJobApplicationsByJobPostId from '~/server/handlers/jobApplication/getJobApplicationsByJobPostId';
+import winstonLogger from '~/server/lib/logger/winston/winstonLogger';
+
+export default defineEventHandler(async (event) => {
+  const { method, originalUrl } = event.node.req;
+
+  // This handler gets job applications by their matching jon post id from the database.
+  if (method === 'GET') {
+    try {
+      return await getJobApplicationsByJobPostId(event);
+    } catch (error) {
+      // If an error occurs then log the error and return an unsuccessful statement.
+      const errorMessage = (error as Error).message;
+      winstonLogger.error(
+        `Error in route ${method} ${originalUrl}: ${errorMessage}`,
+      );
+      return error;
+    }
+  }
+});
