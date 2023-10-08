f65e1ccbf4c91998f6fea8a4912b89fe583037d2
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:49:42 +0100

    feat: created handler

---
+++ server/handlers/profile/companies/deleteCompaniesByProfile.ts
@@ -0,0 +1,20 @@
+import { companiesCollection } from '~/server/lib/db/mongodb/collections';
+import isAuthenticated from '../../auth/isAuthenticated';
+import { H3Event, EventHandlerRequest } from 'h3';
+import getSessionUser from '../../auth/getSessionUser';
+import { ObjectId } from 'mongodb';
+
+// This handler finds and deletes all the companies owned by the signed in user from the db.
+
+export default async function deleteCompaniesByProfile(
+  event: H3Event<EventHandlerRequest>,
+) {
+  // STEP 1: Check user is authenticated.
+  await isAuthenticated(event);
+
+  // STEP 2: Get the user id.
+  const { _id: userId } = await getSessionUser(event);
+
+  // STEP 3: Delete company owners by user id.
+  return await companiesCollection.deleteMany({ userId: new ObjectId(userId) });
+}
