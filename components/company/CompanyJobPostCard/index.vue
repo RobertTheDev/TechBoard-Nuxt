1945e708ea1e9ed7e3f0bec741bbe3c7c509ded4
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:02:34 +0100

    feat: created job post card component

---
+++ components/company/CompanyJobPostCard/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink :to="`/job-posts/${jobPost._id}/admin`">
+    <p>{{ jobPost.title }}</p>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+import IJobPost from '~/models/jobPost/interfaces/JobPost';
+
+const jobPost = defineProps<IJobPost>();
+</script>
