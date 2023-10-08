9c774e9411d0076d9803137283940556ba6ed25a
RobertTheDev <robertthedev@gmail.com>
2023-10-08 22:40:41 +0100

    feat: created schema

--- models/companyOwner/validators/createCompanyOwner.schema.ts
+++ models/companyOwner/validators/createCompanyOwner.schema.ts
@@ -1,7 +1,16 @@
-import { object, z } from 'zod';
+import { object, string, z } from 'zod';
 
 // Zod validation schema defines fields required for creating a company owner.
-const createCompanyOwnerSchema = object({});
+const createCompanyOwnerSchema = object({
+  companyId: string({
+    required_error: 'Company ID is required.',
+    invalid_type_error: 'Company ID must be a string.',
+  }).nonempty('Company ID is required.'),
+  userId: string({
+    required_error: 'User ID is required.',
+    invalid_type_error: 'User ID must be a string.',
+  }).nonempty('User ID is required.'),
+});
 
 // Create a TypeScript type from the schema.
 export type CreateCompanyOwnerSchemaType = z.infer<
