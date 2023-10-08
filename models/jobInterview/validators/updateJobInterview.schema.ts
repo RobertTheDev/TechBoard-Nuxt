8452674527bbd3b21333ee140103347031512f25
RobertTheDev <robertthedev@gmail.com>
2023-10-08 22:40:15 +0100

    feat: created schema

--- models/jobInterview/validators/updateJobInterview.schema.ts
+++ models/jobInterview/validators/updateJobInterview.schema.ts
@@ -1,7 +1,26 @@
-import { object, z } from 'zod';
+import { object, string, z } from 'zod';
+import jobInterviewStatusTypes from '../statusTypes/jobInterviewStatusTypes';
 
 // Zod validation schema defines fields required for updating a job interview.
-const updateJobInterviewSchema = object({});
+const updateJobInterviewSchema = object({
+  startTime: string({
+    invalid_type_error: 'Start time is a string.',
+  })
+    .datetime('Start time must in date time format.')
+    .optional(),
+  endTime: string({
+    invalid_type_error: 'End time is a string.',
+  })
+    .datetime('Start time must in date time format.')
+    .optional(),
+  status: string({
+    invalid_type_error: 'Job interview must be a string.',
+  })
+    .refine((value) => jobInterviewStatusTypes.includes(value), {
+      message: 'Invalid status. Allowed values are BOOKED or CANCELLED.',
+    })
+    .optional(),
+});
 
 // Create a TypeScript type from the schema.
 export type UpdateJobInterviewSchemaType = z.infer<
