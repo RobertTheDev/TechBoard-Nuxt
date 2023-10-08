66fffad1467517bd19c909e6877e0bfd5ccf6fda
RobertTheDev <robertthedev@gmail.com>
2023-10-08 16:59:55 +0100

    feat: added is company owner handler

--- server/handlers/company/updateCompanyById.ts
+++ server/handlers/company/updateCompanyById.ts
@@ -2,16 +2,22 @@ import { H3Event, EventHandlerRequest } from 'h3';
 import { ObjectId } from 'mongodb';
 import updateCompanySchema from '~/models/company/validators/updateCompany.schema';
 import { companiesCollection } from '~/server/lib/db/mongodb/collections';
+import isAuthenticated from '../auth/isAuthenticated';
+import isCompanyOwner from '../companyOwner/isCompanyOwner';
 
 // This handler validates the request body and updates a company by its unique id.
 
 export default async function updateCompanyById(
   event: H3Event<EventHandlerRequest>,
 ) {
-  // STEP 1: Validate the request body.
-  const body = await readBody(event);
+  // STEP 1: Check user is authenticated.
+  await isAuthenticated(event);
 
-  const { id } = event.context.params as { id: string };
+  // STEP 2: Check user is a company owner.
+  await isCompanyOwner(event);
+
+  // STEP 3: Validate the request body.
+  const body = await readBody(event);
 
   const validation = await updateCompanySchema.safeParseAsync(body);
 
@@ -22,7 +28,9 @@ export default async function updateCompanyById(
     });
   }
 
-  // STEP 2: Update the company by its id with the validated data.
+  // STEP 4: Update the company by its id with the validated data.
+  const { id } = event.context.params as { id: string };
+
   const updatedCompany = await companiesCollection.findOneAndUpdate(
     { _id: new ObjectId(id) },
     { $set: validation.data },
@@ -35,7 +43,7 @@ export default async function updateCompanyById(
     });
   }
 
-  // STEP 3: Find and return the company by its unique id.
+  // STEP 5: Find and return the company by its unique id.
   const company = await companiesCollection.findOne({
     _id: new ObjectId(updatedCompany._id),
   });
