f0c90ec7b245763c7ea0d3f851e2980be64cb019
RobertTheDev <robertthedev@gmail.com>
2023-10-08 22:50:48 +0100

    feat: added seo title

--- pages/job-posts/[id]/admin/index.vue
+++ pages/job-posts/[id]/admin/index.vue
@@ -5,3 +5,17 @@
     </div>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Job Post Admin | ${companyName}`,
+  ogTitle: `Job Post Admin | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
