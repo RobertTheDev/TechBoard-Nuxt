e5491dc0521438be5f7c58098fc0daff69682446
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:03:23 +0100

    feat: created job post admin menu component

---
+++ components/jobPost/JobPostAdminMenu/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col">
+    <NuxtLink
+      v-for="jobPostAdminLink in jobPostAdminLinks"
+      :key="jobPostAdminLink.path"
+      :to="`/job-posts/${jobPostId}/admin/${jobPostAdminLink.path}`"
+    >
+      {{ jobPostAdminLink.name }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import jobPostAdminLinks from '~/lib/links/jobPostAdminLinks';
+
+const { id: jobPostId } = useRoute().params;
+</script>
