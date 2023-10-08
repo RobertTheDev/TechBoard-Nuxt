3e9a4ab5be955980354860bb9535df50641668f5
RobertTheDev <robertthedev@gmail.com>
2023-10-08 22:39:27 +0100

    feat: created schema

--- models/jobOffer/validators/updateJobOffer.schema.ts
+++ models/jobOffer/validators/updateJobOffer.schema.ts
@@ -1,7 +1,17 @@
-import { object, z } from 'zod';
+import { object, string, z } from 'zod';
+import jobOfferStatusTypes from '../statusTypes/jobOfferStatusTypes';
 
 // Zod validation schema defines fields required for updating a job offer.
-const updateJobOfferSchema = object({});
+const updateJobOfferSchema = object({
+  status: string({
+    invalid_type_error: 'Job offer status must be a string.',
+  })
+    .refine((value) => jobOfferStatusTypes.includes(value), {
+      message:
+        'Invalid status. Allowed values are ACCEPTED, OFFERED or REJECTED.',
+    })
+    .optional(),
+});
 
 // Create a TypeScript type from the schema.
 export type UpdateJobOfferSchemaType = z.infer<typeof updateJobOfferSchema>;
