260389343f87cda766635cf4738558a811171135
RobertTheDev <robertthedev@gmail.com>
2023-10-08 17:04:24 +0100

    feat: created close job post page

---
+++ pages/job-posts/[id]/admin/close-job-post.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLayout name="job-post-admin">
+    <div>
+      <h1>Close Job Post</h1>
+      <CloseJobPostForm />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import companyName from '~/lib/constants/companyName';
+import logoImage from '~/lib/constants/logoImage';
+
+useSeoMeta({
+  title: `Close Job Post | ${companyName}`,
+  ogTitle: `Close Job Post | ${companyName}`,
+  description: ``,
+  ogDescription: ``,
+  ogImage: logoImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
